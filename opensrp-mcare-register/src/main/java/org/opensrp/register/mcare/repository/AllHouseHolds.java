f68228665d4b997325492509c29d7002e88e4f01
Julkar Nain <julkar@mpower-social.com>
2015-08-27 18:05:50 +0600

    Julkar : Have changed in AllHouseHolds class in allHHsVisited7Days method to set days range

--- opensrp-mcare-register/src/main/java/org/opensrp/register/mcare/repository/AllHouseHolds.java
+++ opensrp-mcare-register/src/main/java/org/opensrp/register/mcare/repository/AllHouseHolds.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.ektorp.CouchDbConnector;
 import org.ektorp.support.GenerateView;
 import org.ektorp.support.View;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.motechproject.dao.MotechBaseRepository;
 import org.opensrp.common.AllConstants;
 import org.opensrp.register.mcare.domain.HouseHold;
@@ -20,7 +22,8 @@ public class AllHouseHolds extends MotechBaseRepository<HouseHold> {
 	private static Logger logger = LoggerFactory.getLogger(AllHouseHolds.class);
 
 	@Autowired
-	public AllHouseHolds(@Qualifier(AllConstants.OPENSRP_DATABASE_CONNECTOR) CouchDbConnector db) {
+	public AllHouseHolds(
+			@Qualifier(AllConstants.OPENSRP_DATABASE_CONNECTOR) CouchDbConnector db) {
 		super(HouseHold.class, db);
 	}
 
@@ -32,26 +35,30 @@ public class AllHouseHolds extends MotechBaseRepository<HouseHold> {
 		}
 		return houseHolds.get(0);
 	}
-	
-	 @View(name = "all_households",
-	            map = "function(doc) { if (doc.type === 'HouseHold') { emit(doc.PROVIDERID, doc.CASEID); } }")
-	    public List<HouseHold> findAllHouseHolds() {
-	        return db.queryView(createQuery("all_households")
-	                .includeDocs(true), 
-	                 HouseHold.class);
-	    }
-	 @View(name = "all_open_hhs_for_provider",
-	            map = "function(doc) { if (doc.type === 'HouseHold' && doc.PROVIDERID) { emit(doc.PROVIDERID); } }")
-	    public List<HouseHold> allOpenHHsForProvider(String providerId) {
-	        return db.queryView(createQuery("all_open_hhs_for_provider")
-	                .key(providerId)
-	                .includeDocs(true), HouseHold.class);
-	    }
-	 @View(name = "all_hhs_prev_7_days",
-	            map = "function(doc) { if (doc.type === 'HouseHold' && doc.PROVIDERID) { emit(doc.PROVIDERID); } }")
-	    public List<HouseHold> allHHsVisited7Days(String providerId) {
-	        return db.queryView(createQuery("all_hhs_prev_7_days")
-	                .key(providerId)
-	                .includeDocs(true), HouseHold.class);
-	    }
+
+	@View(name = "all_households", map = "function(doc) { if (doc.type === 'HouseHold') { emit(doc.PROVIDERID, doc.CASEID); } }")
+	public List<HouseHold> findAllHouseHolds() {
+		return db.queryView(createQuery("all_households").includeDocs(true),
+				HouseHold.class);
+	}
+
+	@View(name = "all_open_hhs_for_provider", map = "function(doc) { if (doc.type === 'HouseHold' && doc.PROVIDERID) { emit(doc.PROVIDERID); } }")
+	public List<HouseHold> allOpenHHsForProvider(String providerId) {
+		return db.queryView(
+				createQuery("all_open_hhs_for_provider").key(providerId)
+						.includeDocs(true), HouseHold.class);
+	}
+
+	@View(name = "all_hhs_prev_7_days", map = "function(doc) { if (doc.type === 'HouseHold' && doc.PROVIDERID && doc.TODAY) { emit(doc.PROVIDERID, doc.TODAY); } }")
+	public List<HouseHold> allHHsVisited7Days(String providerId) {
+
+		LocalDate today = LocalDate.now();
+
+		return db.queryView(
+				createQuery("all_hhs_prev_7_days")
+						.startKey(today.minusDays(100))
+						.endKey(today)
+						//.key(providerId)
+						.includeDocs(true), HouseHold.class);
+	}
 }
