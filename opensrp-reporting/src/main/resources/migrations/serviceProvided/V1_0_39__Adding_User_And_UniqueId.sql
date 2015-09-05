bd55e2625ea2fb01a1cd44bd5e7d7026523878a3
Dimas <dimas.ciputra@gmail.com>
2015-09-05 18:57:12 +0800

    dim | modify last_id in uniqueId table to >=1000

--- opensrp-reporting/src/main/resources/migrations/serviceProvided/V1_0_39__Adding_User_And_UniqueId.sql
+++ opensrp-reporting/src/main/resources/migrations/serviceProvided/V1_0_39__Adding_User_And_UniqueId.sql
@@ -1,5 +1,5 @@
-INSERT INTO report.unique_id(anm_id, last_value) VALUES ((SELECT ID FROM report.dim_anm WHERE anmidentifier='demo1'), 100);
+INSERT INTO report.unique_id(anm_id, last_value) VALUES ((SELECT ID FROM report.dim_anm WHERE anmidentifier='demo1'), 1000);
 
-INSERT INTO report.unique_id(anm_id, last_value) VALUES ((SELECT ID FROM report.dim_anm WHERE anmidentifier='demo2'), 200);
+INSERT INTO report.unique_id(anm_id, last_value) VALUES ((SELECT ID FROM report.dim_anm WHERE anmidentifier='demo2'), 2000);
 
-INSERT INTO report.unique_id(anm_id, last_value) VALUES ((SELECT ID FROM report.dim_anm WHERE anmidentifier='demo3'), 300);
+INSERT INTO report.unique_id(anm_id, last_value) VALUES ((SELECT ID FROM report.dim_anm WHERE anmidentifier='demo3'), 3000);
