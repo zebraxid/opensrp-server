0552401f419fb27d588e4137578df51df52a15ba
bitaron <bitaron90@gmail.com>
2017-03-20 11:51:05 +0600

    New: Add DHIS2 constant file
    Move DHIS2 relate constants like dateset id, data element id, org id etc to a separate file.

---
+++ opensrp-connector/src/main/java/org/opensrp/connector/DHIS2/DHIS2Constants.java
@@ -0,0 +1,24 @@
+package org.opensrp.connector.DHIS2;
+
+public final class DHIS2Constants {
+	public final static String ORG_UNIT = "PKTk8zxbl0J";
+	
+	
+	
+	public final static class DataSet {
+		public final static String FAMILY_PLANNING = "Z5WPr2zconV";
+	}
+
+	public final static class DataElement{
+		public final static String BIRTH_CONTROLL_PILL = "Kt4PihqO6KL";
+	}
+
+	public final static class CategoryOptionElement {
+		public final static String NEW = "DHJ5tZVSSsl";
+		public final static String ABSOULATE_NEW = "UI7LOrrDRyJ";
+		public final static String UNIT_TOTAL = "Y2nkJYOsPfL";
+		public final static String LEFT_NONE_TAKEN = "ip04bzGTrPX";
+		public final static String LEFT_NEW_TAKEN = "zmioNFEGCmE";
+	}
+}
+
