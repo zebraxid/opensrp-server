3fc3eccdb6b219419bdca8395cb67a5a07bd1351
bitaron <bitaron90@gmail.com>
2017-04-11 14:32:42 +0600

    Modify

--- travis_couchdb_lucene.sh
+++ travis_couchdb_lucene.sh
@@ -11,12 +11,11 @@ sed -e 's/localhost:5984/127.0.0.1:5984/' -i couchdb-lucene-1.1.0/conf/couchdb-l
 sudo updatedb
 
 couchdb_local_ini_location="$(locate couchdb/local.ini)"
-python_location="$(which python)"
 
 sudo sed -i -e '$a [couchdb]' $couchdb_local_ini_location
 sudo sed -i -e '$a os_process_timeout=60000 ; increase the timeout from 5 seconds. ' $couchdb_local_ini_location
 sudo sed -i -e '$a [external]' $couchdb_local_ini_location
-sudo sed -i -e '$a fti=$python_location /home/travis/build/OpenSRP/opensrp-server/couchdb-lucene-1.1.0/target/couchdb-lucene-1.1.0/tools/couchdb-external-hook.py' $couchdb_local_ini_location
+sudo sed -i -e '$a fti=/usr/bin/python /home/travis/build/OpenSRP/opensrp-server/couchdb-lucene-1.1.0/target/couchdb-lucene-1.1.0/tools/couchdb-external-hook.py' $couchdb_local_ini_location
 sudo sed -i -e '$a [httpd_db_handlers]' $couchdb_local_ini_location
 sudo sed -i -e '$a _fti = {couch_httpd_external, handle_external_req, <<"fti">>}' $couchdb_local_ini_location
 sudo sed -i -e '$a [httpd_global_handlers]' $couchdb_local_ini_location
