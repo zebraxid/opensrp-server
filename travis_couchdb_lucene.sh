48aa47f609ad19b3af185f9f38e6edc5b97119f4
bitaron <bitaron90@gmail.com>
2017-04-11 13:21:06 +0600

    Modify: Find couchdb local.ini file dynamically

--- travis_couchdb_lucene.sh
+++ travis_couchdb_lucene.sh
@@ -8,13 +8,15 @@ unzip couchdb-lucene-1.1.0-dist.zip
 sed -e 's/^host=localhost$/host=0.0.0.0/' -i couchdb-lucene-1.1.0/conf/couchdb-lucene.ini
 sed -e 's/localhost:5984/127.0.0.1:5984/' -i couchdb-lucene-1.1.0/conf/couchdb-lucene.ini
 
-sudo sed -i -e '$a [couchdb]' /usr/local/etc/couchdb/local.ini
-sudo sed -i -e '$a os_process_timeout=60000 ; increase the timeout from 5 seconds. ' /usr/local/etc/couchdb/local.ini
+couchdb_local_ini_location="$(locate couchdb/local.ini)"
+
+sudo sed -i -e '$a [couchdb]' $couchdb_local_ini_location
+sudo sed -i -e '$a os_process_timeout=60000 ; increase the timeout from 5 seconds. ' $couchdb_local_ini_location
 sudo sed -i -e '$a [external]' /usr/local/etc/couchdb/local.ini
-sudo sed -i -e '$a fti=/usr/bin/python /opt/couchdb-lucene/tools/couchdb-external-hook.py' /usr/local/etc/couchdb/local.ini
-sudo sed -i -e '$a [httpd_db_handlers]' /usr/local/etc/couchdb/local.ini
-sudo sed -i -e '$a _fti = {couch_httpd_external, handle_external_req, <<"fti">>}' /usr/local/etc/couchdb/local.ini
-sudo sed -i -e '$a [httpd_global_handlers]' /usr/local/etc/couchdb/local.ini
-sudo sed -i -e '$a _fti = {couch_httpd_proxy, handle_proxy_req, <<"http:\/\/127.0.0.1:5985">>}' /usr/local/etc/couchdb/local.ini
+sudo sed -i -e '$a fti=/usr/bin/python /opt/couchdb-lucene/tools/couchdb-external-hook.py' $couchdb_local_ini_location
+sudo sed -i -e '$a [httpd_db_handlers]' $couchdb_local_ini_location
+sudo sed -i -e '$a _fti = {couch_httpd_external, handle_external_req, <<"fti">>}' $couchdb_local_ini_location
+sudo sed -i -e '$a [httpd_global_handlers]' $couchdb_local_ini_location
+sudo sed -i -e '$a _fti = {couch_httpd_proxy, handle_proxy_req, <<"http:\/\/127.0.0.1:5985">>}' $couchdb_local_ini_location
 
 
