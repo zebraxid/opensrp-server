f4709139bfdabb25b8874e359d83d77f7a06cc25
bitaron <bitaron90@gmail.com>
2017-04-11 13:30:46 +0600

    Modify: Add sudo for locate command

--- travis_couchdb_lucene.sh
+++ travis_couchdb_lucene.sh
@@ -10,7 +10,7 @@ sed -e 's/localhost:5984/127.0.0.1:5984/' -i couchdb-lucene-1.1.0/conf/couchdb-l
 
 sudo apt-get install mlocate
 
-couchdb_local_ini_location="$(locate couchdb/local.ini)"
+couchdb_local_ini_location="$(sudo locate couchdb/local.ini)"
 
 sudo sed -i -e '$a [couchdb]' $couchdb_local_ini_location
 sudo sed -i -e '$a os_process_timeout=60000 ; increase the timeout from 5 seconds. ' $couchdb_local_ini_location
