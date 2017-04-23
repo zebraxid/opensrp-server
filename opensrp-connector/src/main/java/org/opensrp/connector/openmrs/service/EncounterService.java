3aae3bbae61d3fd44d48e665c7794f5aa3b147a8
Proshanto <proshanto@mpower-social.com>
2017-04-23 18:00:46 +0600

    Work on vaccine encounter

--- opensrp-connector/src/main/java/org/opensrp/connector/openmrs/service/EncounterService.java
+++ opensrp-connector/src/main/java/org/opensrp/connector/openmrs/service/EncounterService.java
@@ -53,6 +53,7 @@ public class EncounterService extends OpenmrsService{
 		this.userService = userService;
 	}
 	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public JSONObject createEncounter(Event e,String idGen) throws JSONException{
 		JSONObject pt = patientService.getPatientByIdentifier(idGen);
 		JSONObject enc = new JSONObject();
@@ -72,10 +73,11 @@ public class EncounterService extends OpenmrsService{
 			enc.put("provider", pr.getString("uuid"));
 
 		try{
-			List<Obs> ol = e.getObs();
+			List<Obs> ol = e.getObs();	
+			System.err.println("ol:"+ol.toString());
 			Map<String, List<JSONObject>> pc = new HashMap<>();
 			MultiValueMap   obsMap = new MultiValueMap();
-					
+			String parentCode="";		
 			for (Obs obs : ol) {	
 				//if no parent simply make it root obs
 				if(StringUtils.isEmptyOrWhitespaceOnly(obs.getParentCode())){
@@ -85,6 +87,7 @@ public class EncounterService extends OpenmrsService{
 					obsMap.put(obs.getParentCode(), convertObsToJson(getOrCreateParent(ol, obs)));
 					// find if any other exists with same parent if so add to the list otherwise create new list
 					List<JSONObject> obl = pc.get(obs.getParentCode());
+					parentCode = obs.getParentCode();
 					if(obl == null){
 						obl = new ArrayList<>();
 					}
@@ -93,49 +96,125 @@ public class EncounterService extends OpenmrsService{
 				}
 			}
 			
+			
 			JSONArray obar = new JSONArray();
-			List<JSONObject> list;
-	        @SuppressWarnings("unchecked")
-			Set <String> entrySet = obsMap.entrySet();
+			List<JSONObject> list;	       
+			Set <String> entrySet = obsMap.entrySet();	
+			System.out.println("obsMap:"+obsMap.toString());
 	        @SuppressWarnings("rawtypes")
 			Iterator it = entrySet.iterator();
+	        System.err.println("entrySet:"+entrySet.toString());
+	        int i=0;
 	        while (it.hasNext()) {
+	        	
 	            Map.Entry mapEntry = (Map.Entry) it.next();
+	            System.out.println("mapEntry.getKey():"+mapEntry.getKey());
 	            list = (List) obsMap.get(mapEntry.getKey());
-	            for (int j = 0; j < list.size(); j++) {  
-	            	JSONObject obo = list.get(j);  	
-	                List<JSONObject> cob = pc.get(mapEntry.getKey());
-	    			if(cob != null && cob.size() > 0) {
+	           // for (int j = 0; j < list.size(); j++) {  
+	            	JSONObject obo = list.get(i); 	            	
+	                List<JSONObject> cob = pc.get(mapEntry.getKey());	               
+	    			if(cob != null && cob.size() > 0) {	    				
 	    				obo.put("groupMembers", new JSONArray(cob));
 	    			}
 	    			obar.put(obo);
-	            }
+	            //}
+	    			i++;
+	    			 System.err.println("obar:"+obar);	
 	        }
-	        
 	       
-			enc.put("obs", obar);
+	       System.out.println("parentCode:"+parentCode);
+	      return  this.createVaccineEncounter(obar, enc,parentCode);
+	       
 		}catch(Exception ee){
-			System.out.println(ee.getMessage());
+			ee.printStackTrace();
 		}
 		System.out.println("Going to create Encounter: " + enc.toString());
 		HttpResponse op = HttpUtil.post(HttpUtil.removeEndingSlash(OPENMRS_BASE_URL)+"/"+ENCOUNTER_URL, "", enc.toString(), OPENMRS_USER, OPENMRS_PWD);
 		return new JSONObject(op.body());
+		
 	}
 	
+	@SuppressWarnings("unchecked")
+	private JSONObject createVaccineEncounter(JSONArray obar,JSONObject enc,String parentCode){
+		try{
+			System.out.println("obar:"+obar);
+			String getVaccinesAsString =  obar.get(0).toString();
+	        JSONObject convertVaccinesJsonObjectFromString = new JSONObject(getVaccinesAsString);
+	        String convertVaccinesJsonObjectToRawString = convertVaccinesJsonObjectFromString.get("value").toString();
+	        String groupMembers =  obar.get(1).toString();
+	        System.out.println("groupMembers:"+groupMembers.toString());
+	        JSONObject groupMembersObject = new JSONObject(groupMembers);
+	        JSONArray groupMembersList =  (JSONArray) groupMembersObject.get("groupMembers");       
+	        String[] vaccineList = convertVaccinesJsonObjectToRawString.split(" ");
+	        System.out.println("vaccineList:"+vaccineList);
+	        @SuppressWarnings("rawtypes")
+			Map map=new HashMap();  
+			map.put("TT1","c8e8ed67-d125-4c77-a8c7-2c9a5cf2c46b");  
+		    map.put("TT2","14f59334-bde6-4512-bfae-830d5bd7da87");
+		    map.put("TT3","8197f5b8-328c-402d-8128-fd04d9de8a94"); 
+		    map.put("TT4","4914c323-d32d-4412-905f-c8c90f92136f"); 
+		    map.put("TT5","4dd71b8e-8109-4eb3-b919-186f8236edb1");
+		    
+		    map.put("bcg","1cf49965-5130-4648-8473-37238d24b826");
+		    map.put("opv0","548a2d2d-4803-4d9a-8d78-8a31741baccc");
+		    map.put("pcv1","e3340e49-df17-4b86-841e-6a207db40e58");
+		    map.put("opv1","548a2d2d-4803-4d9a-8d78-8a31741baccc");
+		    map.put("penta1","e601701f-a9c5-4cea-a06e-4e5abacaf6e4");
+		    map.put("pcv2","27c360d3-ce24-492d-a004-1d551dfd0933");
+		    map.put("opv2","548a2d2d-4803-4d9a-8d78-8a31741baccc");
+		    map.put("penta2","9fbd9d94-0634-4880-954e-502c8be1c2d4");
+		    map.put("pcv3","b0735577-6747-4c3e-bb52-b2d9b0d2419a");
+		    map.put("opv3","548a2d2d-4803-4d9a-8d78-8a31741baccc");
+		    map.put("penta3","8c0268d5-befe-47e3-a98d-d7d13f214f5b");
+		    map.put("ipv","caffb48d-4155-47a5-a81a-ef669f0585d0");
+		    map.put("measles1","9c23d2fe-e2db-48ea-98ac-cfb832f7c0b6");
+		    map.put("measles1","d2c8d3fa-495a-4e5f-8e8b-3c598725c0c3");
+
+		    
+		    int indicator=0;	        
+		    for (int j = 0; j < vaccineList.length; j++) {				
+	        	JSONArray gruopMember = new JSONArray();
+	 	        JSONArray observation = new JSONArray();	 	       
+	 	        JSONObject groupObservation = new JSONObject();	
+	 	        JSONObject observationConcept = new JSONObject(); 
+	 	        System.out.println("vaccineList[j]:"+vaccineList[j]);
+	 	        observationConcept.put("concept", convertVaccinesJsonObjectFromString.get("concept"));
+	 	        observationConcept.put("value", map.get(vaccineList[j]));
+	        	groupMembersList.get(indicator);
+	        	groupMembersList.get(indicator+1);
+	        	gruopMember.put(groupMembersList.get(indicator));
+	        	gruopMember.put(groupMembersList.get(indicator+1));
+	        	groupObservation.put("groupMembers", gruopMember);
+	        	groupObservation.put("concept", parentCode);
+	        	indicator+=2;	        	
+	        	observation.put(observationConcept);
+	        	observation.put(groupObservation);	 	        
+	 	        enc.put("obs", observation);
+	 	        System.out.println("Going to create Encounter: " + enc.toString());
+	 	        HttpResponse op = HttpUtil.post(HttpUtil.removeEndingSlash(OPENMRS_BASE_URL)+"/"+ENCOUNTER_URL, "", enc.toString(), OPENMRS_USER, OPENMRS_PWD);
+	 	        System.out.println(new JSONObject(op.body()));
+		    }
+       
+			}catch(Exception ee){
+				ee.printStackTrace();
+			}
+		return null;
+		
+	}
 	private JSONObject convertObsToJson(Obs o) throws JSONException{
 		JSONObject obo = new JSONObject();
 		obo.put("concept", o.getFieldCode());
 		if(o.getValue() != null && !StringUtils.isEmptyOrWhitespaceOnly(o.getValue().toString())) {
-			if(o.getFieldCode().toString().equalsIgnoreCase("163137AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") || o.getFieldCode().toString().equalsIgnoreCase("163138AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") || o.getFieldCode().toString().equalsIgnoreCase("5599AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") || o.getFieldCode().toString().equalsIgnoreCase("5596AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"))
-			{	
+			/*if(o.getFieldCode().toString().equalsIgnoreCase("163137AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") || o.getFieldCode().toString().equalsIgnoreCase("163138AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") || o.getFieldCode().toString().equalsIgnoreCase("5599AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") || o.getFieldCode().toString().equalsIgnoreCase("5596AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"))
+			{*/	
 				//obo.put("value", OPENMRS_DATETime.format(o.getValue()));
 				if (o.getValue().toString().length() >= 19)
 					obo.put("value", (o.getValue().toString().substring(0, 19)).replace("T", " "));
 				else 
 					obo.put("value", o.getValue());
-			}			
+			/*}			
 			else 
-				obo.put("value", o.getValue());
+				obo.put("value", o.getValue());*/
 		}
 		
 		return obo;
--- opensrp-mcare-register/src/test/java/org/opensrp/register/mcare/repository/it/AllMemberTest.java
+++ opensrp-mcare-register/src/test/java/org/opensrp/register/mcare/repository/it/AllMemberTest.java
@@ -1,5 +1,8 @@
 package org.opensrp.register.mcare.repository.it;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.ektorp.CouchDbInstance;
 import org.ektorp.http.HttpClient;
 import org.ektorp.http.StdHttpClient;
@@ -53,23 +56,206 @@ public class AllMemberTest {
         System.err.println(houseHold.HoH_Mobile_No());
 		
 	}
-	
 	@Ignore@Test
+	public void bahmniEncounterTest() throws JSONException{
+		JSONObject enc = new JSONObject();
+		enc.put("encounterTypeUuid", "910d3315-0632-4c5b-9e2a-32916d0a8004");
+		enc.put("encounterDateTime", "2017-04-20T10:19:18.000+0600");
+		enc.put("patientUuid", "00f88b53-0b98-4fee-b09f-bd612dd9fc47");
+		
+		JSONObject providers = new JSONObject();
+		JSONArray providerArray = new JSONArray();
+		providers.put("uuid", "dd967c60-5089-4e49-82be-ef7990439619");
+		providerArray.put(providers);
+		
+		JSONObject observations = new JSONObject();
+		JSONObject concept = new JSONObject();
+		concept.put("uuid", "021cb967-953d-11e6-90c1-005056b01095");//Immunization Incident Template
+		
+		///inner group members
+		
+		JSONArray innerGroupMember = new JSONArray();
+		
+		
+		/// group memener 1
+		JSONObject innerGroupMember1 = new JSONObject();
+		JSONObject innerGroupMember1Concept = new JSONObject();
+		innerGroupMember1Concept.put("uuid", "021e0705-953d-11e6-90c1-005056b01095");//Immunization Incident Vaccine
+		innerGroupMember1.put("concept", innerGroupMember1Concept);
+		
+		
+		JSONObject innerGroupMember1Value = new JSONObject();		
+		innerGroupMember1Value.put("uuid", "9c6b6047-137c-48fe-8955-3ceea1bd1327");	//TT 3	
+		innerGroupMember1.put("value", innerGroupMember1Value);
+		
+		
+		
+		/// group member 2
+		JSONObject innerGroupMember2 = new JSONObject();
+		JSONObject innerGroupMember2Concept = new JSONObject();
+		innerGroupMember2Concept.put("uuid", "021efde7-953d-11e6-90c1-005056b01095");//Immunization Incident Vaccination Dosage
+		innerGroupMember2.put("concept", innerGroupMember2Concept);		
+		innerGroupMember2.put("value", 1);
+		
+		
+		/// group member 2
+		JSONObject innerGroupMember3 = new JSONObject();
+		JSONObject innerGroupMember3Concept = new JSONObject();
+		innerGroupMember3Concept.put("uuid", "021f7622-953d-11e6-90c1-005056b01095");
+		innerGroupMember3.put("concept", innerGroupMember3Concept);		
+		innerGroupMember3.put("value", true);
+		
+		innerGroupMember.put(innerGroupMember1);
+		innerGroupMember.put(innerGroupMember2);
+		
+		
+		// group member property
+		JSONObject groupMember = new JSONObject();
+		groupMember.put("groupMembers", innerGroupMember);
+		
+		JSONObject conceptInner = new JSONObject();
+		conceptInner.put("uuid", "021d28cd-953d-11e6-90c1-005056b01095"); //Immunization Incident Group
+		groupMember.put("concept", conceptInner);
+		
+		
+		
+		// group member array property
+		JSONArray groupMemberArray = new JSONArray();
+		groupMemberArray.put(groupMember);
+		
+		JSONArray observationsArray = new JSONArray();
+		observations.put("conceptSetName", "Immunization Incident Group");
+		observations.put("concept", concept);
+		observations.put("groupMembers", groupMemberArray);
+		
+		observationsArray.put(observations);
+		
+		enc.put("providers", providerArray);
+		enc.put("observations", observationsArray);
+		System.out.println("Going to create Encounter: " + enc.toString());
+		
+		
+		HttpResponse op = HttpUtil.post("https://103.247.238.26/openmrs/ws/rest/v1/bahmnicore/bahmniencounter", "", enc.toString(),"sohel", "Sohel@123");
+		System.out.println(op.getClass());
+		
+	}
+	
+	
+	/*@Test
+	public void bahmniEncounterTest() throws JSONException{
+		JSONObject enc = new JSONObject();
+		enc.put("encounterTypeUuid", "910d3315-0632-4c5b-9e2a-32916d0a8004");
+		enc.put("encounterDateTime", "2017-04-20T10:19:18.000+0600");
+		enc.put("patientUuid", "00f88b53-0b98-4fee-b09f-bd612dd9fc47");
+		
+		JSONObject providers = new JSONObject();
+		JSONArray providerArray = new JSONArray();
+		providers.put("uuid", "dd967c60-5089-4e49-82be-ef7990439619");
+		providerArray.put(providers);
+		
+		JSONObject observations = new JSONObject();
+		JSONObject concept = new JSONObject();
+		concept.put("dataType", "N/A");
+		concept.put("name", "Immunization Incident Template");
+		concept.put("uuid", "021cb967-953d-11e6-90c1-005056b01095");
+		
+		///inner group members
+		
+		JSONArray innerGroupMember = new JSONArray();
+		
+		
+		/// group memener 1
+		JSONObject innerGroupMember1 = new JSONObject();
+		JSONObject innerGroupMember1Concept = new JSONObject();
+		//innerGroupMember1Concept.put("dataType", "Coded");
+		innerGroupMember1Concept.put("name", "Immunization Incident Vaccine");
+		innerGroupMember1Concept.put("uuid", "021e0705-953d-11e6-90c1-005056b01095");
+		innerGroupMember1.put("concept", innerGroupMember1Concept);
+		
+		
+		JSONObject innerGroupMember1Value = new JSONObject();
+		//innerGroupMember1Value.put("name", "TT 3");
+		innerGroupMember1Value.put("uuid", "8197f5b8-328c-402d-8128-fd04d9de8a94");
+		//innerGroupMember1Value.put("value", "TT 3");
+		innerGroupMember1.put("value", innerGroupMember1Value);
+		
+		
+		
+		/// group memener 2
+		JSONObject innerGroupMember2 = new JSONObject();
+		JSONObject innerGroupMember2Concept = new JSONObject();
+		//innerGroupMember2Concept.put("dataType", "Numeric");
+		innerGroupMember2Concept.put("name", "Immunization Incident Vaccination Dosage");
+		innerGroupMember2Concept.put("uuid", "021efde7-953d-11e6-90c1-005056b01095");
+		innerGroupMember2.put("concept", innerGroupMember2Concept);
+		
+		innerGroupMember2.put("value", 1);
+		
+		
+		/// group memener 2
+		JSONObject innerGroupMember3 = new JSONObject();
+		JSONObject innerGroupMember3Concept = new JSONObject();
+		//innerGroupMember3Concept.put("dataType", "Boolean");
+		innerGroupMember3Concept.put("name", "Immunization Incident Vaccination Reported");
+		innerGroupMember3Concept.put("uuid", "021f7622-953d-11e6-90c1-005056b01095");
+		innerGroupMember3.put("concept", innerGroupMember3Concept);		
+		innerGroupMember3.put("value", true);
+		
+		
+		
+		
+		innerGroupMember.put(innerGroupMember1);
+		innerGroupMember.put(innerGroupMember2);
+		innerGroupMember.put(innerGroupMember3);
+		
+		// group member property
+		JSONObject groupMember = new JSONObject();
+		groupMember.put("groupMembers", innerGroupMember);
+		groupMember.put("isObservation", true);
+		JSONObject conceptInner = new JSONObject();
+		conceptInner.put("dataType", "N/A");
+		conceptInner.put("name", "Immunization Incident Group");
+		conceptInner.put("uuid", "021d28cd-953d-11e6-90c1-005056b01095");
+		groupMember.put("concept", conceptInner);
+		
+		
+		
+		// group member array property
+		JSONArray groupMemberArray = new JSONArray();
+		groupMemberArray.put(groupMember);
+		
+		JSONArray observationsArray = new JSONArray();
+		observations.put("conceptSetName", "Immunization Incident Group");
+		observations.put("concept", concept);
+		observations.put("groupMembers", groupMemberArray);
+		observations.put("label", "Immunization Incident");
+		observationsArray.put(observations);
+		
+		enc.put("providers", providerArray);
+		enc.put("observations", observationsArray);
+		System.out.println("Going to create Encounter: " + enc.toString());
+		
+		
+		HttpResponse op = HttpUtil.post("https://103.247.238.26/openmrs/ws/rest/v1/bahmnicore/bahmniencounter", "", enc.toString(),"sohel", "Sohel@123");
+		System.out.println(op.getClass());
+		
+	}*/
+	@Test
 	public void EncounterTest() throws JSONException{
 		JSONObject enc = new JSONObject();
 		enc.put("encounterDatetime", "2017-04-05");
 		enc.put("provider", "3e03cbab-bb1b-427c-96c1-3ad4b1d63e9b");
-		enc.put("patient", "5b3430a7-52f1-4d70-8acb-0bc59f455881");
+		enc.put("patient", "00f88b53-0b98-4fee-b09f-bd612dd9fc47");
 		enc.put("encounterType", "Woman TT Follow Up");
 		
-		
+		//021e0705-953d-11e6-90c1-005056b01095
 		JSONArray obar1 = new JSONArray();
 		JSONObject obs1 = new JSONObject();
 		obs1.put("concept", "021e0705-953d-11e6-90c1-005056b01095");
-		obs1.put("value", "TT 1");
-		obar1.put(obs1);
+		obs1.put("value", "c8e8ed67-d125-4c77-a8c7-2c9a5cf2c46b");
+		obar1.put(obs1);		
 		
-		JSONArray obar2 = new JSONArray();
+		
 		JSONObject obs2 = new JSONObject();		
 		JSONArray gruop1 = new JSONArray();		
 		JSONObject ob1 = new JSONObject(); 
@@ -83,9 +269,10 @@ public class AllMemberTest {
 		gruop1.put(ob2);
 		
 		obs2.put("groupMembers", gruop1);
-		obs2.put("concept", "be200e2b-0469-4cb3-b3c3-eeda1c51b336");
+		obs2.put("concept", "021e0705-953d-11e6-90c1-005056b01095");
+		obs2.put("value", "021e0705-953d-11e6-90c1-005056b01095");
 		
-		obar1.put(obs2);
+		//obar1.put(obs2);
 		
 		
 		JSONArray obar3 = new JSONArray();
@@ -115,6 +302,12 @@ public class AllMemberTest {
 		System.out.println("Going to create Encounter: " + enc.toString());
 		HttpResponse op = HttpUtil.post("https://103.247.238.26/openmrs/ws/rest/v1/encounter", "", enc.toString(),"sohel", "Sohel@123");
 		System.out.println(op.getClass());
+		String c = "dd";
+		Map map=new HashMap();  
+		map.put("dd","Amit");  
+	    map.put("df","Rahul");  
+	  
+	    System.out.println( map.get(c));
 	}
 	
 }
