8c0b6c7e30ff390fa5a0a267c8902916572ad91e
bitaron <bitaron90@gmail.com>
2017-04-26 17:19:01 +0600

    New: Add `ANCVisit` and `ANCVisitTestData` class
    ANCVisit class create various anc visits using builder class.

---
+++ opensrp-mcare-register/src/test/java/org/opensrp/register/mcare/report/mis1/ANCVisit.java
@@ -0,0 +1,44 @@
+package org.opensrp.register.mcare.report.mis1;
+
+
+import org.opensrp.register.mcare.domain.Members;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ANCVisit {
+    private Map<String, String> followUp;
+
+    public Map<String, String> getFollowUp() {
+        return followUp;
+    }
+
+    public static class ANCVisitBuilder {
+        private Map<String, String> followUp;
+
+        public ANCVisitBuilder() {
+            followUp = new HashMap<>();
+        }
+
+        public ANCVisit.ANCVisitBuilder clientVersion(long clientVersion) {
+            followUp.put(Members.CLIENT_VERSION_KEY, String.valueOf(clientVersion));
+            return this;
+        }
+        public ANCVisit.ANCVisitBuilder pregnant(String pregnantStatus) {
+            followUp.put(Members.PREGNANT_STATUS_KEY, pregnantStatus);
+            return this;
+        }
+
+        public ANCVisit build() {
+            ANCVisit eligibleCoupleFollowUP = new ANCVisit(this);
+            this.followUp.clear();
+            return eligibleCoupleFollowUP;
+        }
+    }
+
+    private ANCVisit(ANCVisit.ANCVisitBuilder ancVisitBuilder) {
+        this.followUp = new HashMap<>();
+        this.followUp.putAll(ancVisitBuilder.followUp);
+    }
+
+}
---
+++ opensrp-mcare-register/src/test/java/org/opensrp/register/mcare/report/mis1/maternityCare/ANCVisit1TestData.java
@@ -0,0 +1,11 @@
+package org.opensrp.register.mcare.report.mis1.maternityCare;
+
+
+import org.opensrp.register.mcare.report.mis1.MIS1TestData;
+
+public class ANCVisit1TestData extends MIS1TestData {
+
+    public ANCVisit1TestData(int totalCount, int validCount, long startDateTime, long endDateTime) {
+        super(totalCount, validCount, startDateTime, endDateTime);
+    }
+}
