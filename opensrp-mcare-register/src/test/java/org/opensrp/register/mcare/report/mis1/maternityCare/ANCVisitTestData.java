c0fc6f3373fba014d22ecfff9cea7f811407508b
bitaron <bitaron90@gmail.com>
2017-05-15 14:52:32 +0600

    New: Add test data for ANC visit `Is_Reffered` count.

--- opensrp-mcare-register/src/test/java/org/opensrp/register/mcare/report/mis1/maternityCare/ANCVisitTestData.java
+++ opensrp-mcare-register/src/test/java/org/opensrp/register/mcare/report/mis1/maternityCare/ANCVisitTestData.java
@@ -8,7 +8,8 @@ import org.opensrp.register.mcare.report.mis1.MIS1TestData;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.opensrp.register.mcare.domain.Members.BooleanAnswer.*;
+import static org.opensrp.register.mcare.domain.Members.BooleanAnswer.NO;
+import static org.opensrp.register.mcare.domain.Members.BooleanAnswer.YES;
 
 public class ANCVisitTestData extends MIS1TestData {
 
@@ -28,9 +29,9 @@ public class ANCVisitTestData extends MIS1TestData {
 
         for (int i = 0; i < totalCount; i++) {
             if (i < validCount - 2) {
-                allMembers.add(createValidMemberWithRandomDateTime(VisitNumber.one));
+                allMembers.add(createValidMemberWithValidRandomDateTime(VisitNumber.one));
             } else {
-                allMembers.add(createInValidMemberWithRandomDateTime(VisitNumber.one));
+                allMembers.add(createInValidMemberWithInvalidRandomDateTime(VisitNumber.one));
             }
 
         }
@@ -45,9 +46,9 @@ public class ANCVisitTestData extends MIS1TestData {
 
         for (int i = 0; i < totalCount; i++) {
             if (i < validCount - 2) {
-                allMembers.add(createValidMemberWithRandomDateTime(VisitNumber.two));
+                allMembers.add(createValidMemberWithValidRandomDateTime(VisitNumber.two));
             } else {
-                allMembers.add(createInValidMemberWithRandomDateTime(VisitNumber.two));
+                allMembers.add(createInValidMemberWithInvalidRandomDateTime(VisitNumber.two));
             }
 
         }
@@ -62,9 +63,9 @@ public class ANCVisitTestData extends MIS1TestData {
 
         for (int i = 0; i < totalCount; i++) {
             if (i < validCount - 2) {
-                allMembers.add(createValidMemberWithRandomDateTime(VisitNumber.three));
+                allMembers.add(createValidMemberWithValidRandomDateTime(VisitNumber.three));
             } else {
-                allMembers.add(createInValidMemberWithRandomDateTime(VisitNumber.three));
+                allMembers.add(createInValidMemberWithInvalidRandomDateTime(VisitNumber.three));
             }
 
         }
@@ -79,25 +80,29 @@ public class ANCVisitTestData extends MIS1TestData {
 
         for (int i = 0; i < totalCount; i++) {
             if (i < validCount - 2) {
-                allMembers.add(createValidMemberWithRandomDateTime(VisitNumber.four));
+                allMembers.add(createValidMemberWithValidRandomDateTime(VisitNumber.four));
             } else {
-                allMembers.add(createInValidMemberWithRandomDateTime(VisitNumber.four));
+                allMembers.add(createInValidMemberWithInvalidRandomDateTime(VisitNumber.four));
             }
 
         }
         return allMembers;
     }
 
-    public List<Members> createAFCVisitIsReferredData() {
+    public List<Members> createANCVisitIsReferredData() {
         List<Members> allMembers = new ArrayList<>();
 
 
+        for (VisitNumber visitNumber : VisitNumber.values()) {
+            createValidMemberWithStartDateTime(visitNumber);
+            createValidMemberWithEndDateTime(visitNumber);
+        }
 
         for (int i = 0; i < totalCount; i++) {
-            if (i < validCount - 2) {
-                allMembers.add(createValidMemberWithRandomDateTime(VisitNumber.four));
+            if (i < validCount - 8) {
+                allMembers.add(getValidMemberBasedOn(i));
             } else {
-                allMembers.add(createInValidMemberWithRandomDateTime(VisitNumber.four));
+                allMembers.add(getInvalidValidMemberBasedOn(i));
             }
 
         }
@@ -114,21 +119,67 @@ public class ANCVisitTestData extends MIS1TestData {
         return createMemberWithANCVisit(visitNumber, builder.build().getVisitData());
     }
 
-    private Members createValidMemberWithRandomDateTime(VisitNumber visitNumber) {
+    private Members getValidMemberBasedOn(int count) {
+        int mod = count % 4;
+        if (mod == 0) {
+            return createValidMemberWithValidRandomDateTime(VisitNumber.one);
+        } else if (mod == 1) {
+            return createValidMemberWithValidRandomDateTime(VisitNumber.two);
+        } else if (mod == 2) {
+            return createValidMemberWithValidRandomDateTime(VisitNumber.three);
+        } else {
+            return createValidMemberWithValidRandomDateTime(VisitNumber.four);
+        }
+    }
+
+    private Members getInvalidValidMemberBasedOn(int count) {
+        int mod = count % 2;
+        if (mod == 0) {
+            int rand = getRandomNumberBetween(0, 4);
+            if (rand == 0) {
+                return createInValidMemberWithInvalidRandomDateTime(VisitNumber.one);
+            } else if (rand == 1) {
+                return createInValidMemberWithInvalidRandomDateTime(VisitNumber.two);
+            } else if (rand == 2) {
+                return createInValidMemberWithInvalidRandomDateTime(VisitNumber.three);
+            } else {
+               return createInValidMemberWithInvalidRandomDateTime(VisitNumber.four);
+            }
+        } else {
+            int rand = getRandomNumberBetween(0, 4);
+            if (rand == 0) {
+                return createInValidMemberWithValidRandomDateTime(VisitNumber.one);
+            } else if (rand == 1) {
+                return createInValidMemberWithValidRandomDateTime(VisitNumber.two);
+            } else if (rand == 2) {
+                return createInValidMemberWithValidRandomDateTime(VisitNumber.three);
+            } else {
+                return createInValidMemberWithValidRandomDateTime(VisitNumber.four);
+            }
+        }
+    }
+
+    private Members createValidMemberWithValidRandomDateTime(VisitNumber visitNumber) {
         Long randomDateTimeBetweenStartAndEndDateTime = getRandomNumberBetween(startDateTime, endDateTime);
-        builder.clientVersion(randomDateTimeBetweenStartAndEndDateTime);
+        builder.clientVersion(randomDateTimeBetweenStartAndEndDateTime).isReferred(YES.getValueInString());
         return createMemberWithANCVisit(visitNumber, builder.build().getVisitData());
     }
 
-    private Members createInValidMemberWithRandomDateTime(VisitNumber visitNumber) {
+    private Members createInValidMemberWithValidRandomDateTime(VisitNumber visitNumber) {
+        Long randomDateTimeBetweenStartAndEndDateTime = getRandomNumberBetween(startDateTime, endDateTime);
+        builder.clientVersion(randomDateTimeBetweenStartAndEndDateTime).isReferred(NO.getValueInString());
+        return createMemberWithANCVisit(visitNumber, builder.build().getVisitData());
+    }
+
+    private Members createInValidMemberWithInvalidRandomDateTime(VisitNumber visitNumber) {
         int random = getRandomNumberBetween(1, 100);
         long invalidDataTime;
-        if(random % 2 == 0) {
-            invalidDataTime = getRandomNumberBetween(0, startDateTime-1);
-        }else {
-            invalidDataTime = getRandomNumberBetween(endDateTime+1, endDateTime*5);
+        if (random % 2 == 0) {
+            invalidDataTime = getRandomNumberBetween(0, startDateTime - 1);
+        } else {
+            invalidDataTime = getRandomNumberBetween(endDateTime + 1, endDateTime * 5);
         }
-        builder.clientVersion(invalidDataTime);
+        builder.clientVersion(invalidDataTime).isReferred(YES.getValueInString());
         return createMemberWithANCVisit(visitNumber, builder.build().getVisitData());
     }
 
