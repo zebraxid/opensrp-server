41fb6939a73b98723b9f9396c3202ceb05927130
bitaron <bitaron90@gmail.com>
2017-07-02 14:41:44 +0600

    Modify: Add test for `Utils` class.

--- opensrp-core/src/test/java/org/opensrp/util/TestResourceLoader.java
+++ opensrp-core/src/test/java/org/opensrp/util/TestResourceLoader.java
@@ -3,6 +3,9 @@ package org.opensrp.util;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.opensrp.form.domain.FormSubmission;
---
+++ opensrp-core/src/test/java/org/opensrp/util/UtilTest.java
@@ -0,0 +1,72 @@
+package org.opensrp.util;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.opensrp.form.domain.FormSubmission;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.Assert.*;
+
+public class UtilTest extends TestResourceLoader {
+
+    public String declaredFieldOne;
+    public int declaredFieldTwo;
+
+    @Test
+    public void testGetStringFromJson() throws IOException {
+        String formSubmission = "{\"_id\": \"1\",\n" +
+                "   \"_rev\": \"2\"}";
+        Map<String, String> map = Utils.getStringMapFromJSON(formSubmission);
+        assertFalse(map.isEmpty());
+        assertEquals(map.size(), 2);
+        assertEquals(map.get("_id"), "1");
+        assertEquals(map.get("_rev"), "2");
+        assertNull(map.get("sdf"));
+    }
+
+    @Test(expected = Exception.class)
+    public void testErrorForInvalidJsonInGetStringFromJson() {
+        String formSubmission = "Invalid  JSON";
+        Map<String, String> map = Utils.getStringMapFromJSON(formSubmission);
+    }
+
+    @Test
+    public void testGetFieldsAsList() {
+        List<String > fieldList = Utils.getFieldsAsList(UtilTest.class);
+        assertEquals(2, fieldList.size());
+        assertEquals("declaredFieldOne", fieldList.get(0));
+        assertEquals("declaredFieldTwo", fieldList.get(1));
+    }
+
+    @Test
+    public void testGetZiggyParam() throws IOException, JSONException {
+        FormSubmission formSubmission = getFormSubmissionFor("new_household_registration", 1);
+        String ziggyParams = Utils.getZiggyParams(formSubmission);
+        JSONObject jsonObject = new JSONObject(ziggyParams);
+
+        assertEquals(formSubmission.anmId(), jsonObject.get("anmId"));
+        assertEquals(formSubmission.formName(), jsonObject.get("formName"));
+        assertEquals(formSubmission.serverVersion(), Long.parseLong((String)jsonObject.get("serverVersion")));
+        assertEquals(formSubmission.entityId(), jsonObject.get("entityId"));
+        assertEquals(formSubmission.instanceId(), jsonObject.get("instanceId"));
+        assertEquals(formSubmission.clientVersion(),  Long.parseLong((String)jsonObject.get("clientVersion")));
+    }
+
+    @Test
+    public void testGetXlsToJson() throws IOException, JSONException {
+        String path = getFullPath("sampleXls.xls");
+        JSONArray jsonArray = Utils.getXlsToJson(path);
+        JSONObject jsonObject = jsonArray.getJSONObject(0);
+        assertEquals(1, jsonArray.length());
+        assertEquals("f", jsonObject.get("first"));
+        assertEquals("s", jsonObject.get("second"));
+
+    }
+
+}
