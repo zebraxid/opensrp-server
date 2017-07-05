81365f2f2847d2605ea94d05e232eb50ab18c872
bitaron <bitaron90@gmail.com>
2017-07-05 15:59:27 +0600

    Modify: Make literal strings constants.

--- opensrp-interface/src/main/java/org/opensrp/dto/ActionData.java
+++ opensrp-interface/src/main/java/org/opensrp/dto/ActionData.java
@@ -8,38 +8,41 @@ import org.joda.time.DateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.opensrp.dto.ActionData.DataKey.*;
+
 public class ActionData {
     private Map<String, String> data;
     private String target;
     private String type;
     private Map<String, String> details;
 
+
     public static ActionData createAlert(String beneficiaryType, String scheduleName, String visitCode,
                                          AlertStatus alertStatus, DateTime startDate, DateTime expiryDate) {
-        return new ActionData("alert", "createAlert")
-                .with("beneficiaryType", beneficiaryType)
-                .with("scheduleName", scheduleName)
-                .with("visitCode", visitCode)
-                .with("alertStatus", alertStatus.value())
-                .with("startDate", startDate.toLocalDate().toString())
-                .with("expiryDate", expiryDate.toLocalDate().toString());
+        return new ActionData(ALERT, CREATE_ALERT)
+                .with(BENEFICIARY_TYPE, beneficiaryType)
+                .with(SCHEDULE_NAME, scheduleName)
+                .with(VISIT_CODE, visitCode)
+                .with(ALERT_STATUS, alertStatus.value())
+                .with(START_DATE, startDate.toLocalDate().toString())
+                .with(EXPIRY_DATE, expiryDate.toLocalDate().toString());
     }
 
     public static ActionData markAlertAsClosed(String visitCode, String completionDate) {
-        return new ActionData("alert", "closeAlert")
-                .with("visitCode", visitCode)
-                .with("completionDate", completionDate);
+        return new ActionData(ALERT, CLOSE_ALERT)
+                .with(VISIT_CODE, visitCode)
+                .with(COMPLETION_DATE, completionDate);
     }
 
     public static ActionData reportForIndicator(String indicator, String annualTarget, String monthSummaries) {
-        return new ActionData("report", indicator)
-                .with("annualTarget", annualTarget)
-                .with("monthlySummaries", monthSummaries);
+        return new ActionData(REPORT, indicator)
+                .with(ANNUAL_TARGET, annualTarget)
+                .with(MONTHLY_SUMMARIES, monthSummaries);
     }
 
     public static ActionData closeBeneficiary(String target, String reasonForClose) {
-        return new ActionData(target, "close")
-                .with("reasonForClose", reasonForClose);
+        return new ActionData(target, CLOSE)
+                .with(REASON_FOR_CLOSE, reasonForClose);
     }
 
     public static ActionData from(String actionType, String actionTarget, Map<String, String> data, Map<String, String> details) {
@@ -90,5 +93,24 @@ public class ActionData {
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this);
+    }
+
+
+    static class DataKey {
+        public static final String START_DATE = "startDate";
+        public static final String EXPIRY_DATE = "expiryDate";
+        public static final String COMPLETION_DATE = "completionDate";
+        public static final String REPORT = "report";
+        public static final String ANNUAL_TARGET = "annualTarget";
+        public static final String MONTHLY_SUMMARIES = "monthlySummaries";
+        public static final String CLOSE = "close";
+        public static final String REASON_FOR_CLOSE = "reasonForClose";
+        public static final String VISIT_CODE = "visitCode";
+        public static final String ALERT_STATUS = "alertStatus";
+        public static final String SCHEDULE_NAME = "scheduleName";
+        public static final String BENEFICIARY_TYPE = "beneficiaryType";
+        public static final String ALERT = "alert";
+        public static final String CREATE_ALERT = "createAlert";
+        public static final String CLOSE_ALERT = "closeAlert";
     }
 }
