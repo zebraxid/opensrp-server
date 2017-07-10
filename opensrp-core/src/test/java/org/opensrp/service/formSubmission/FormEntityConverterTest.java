0148bfaa89eabac1b217522db740b788ae26ef6e
bitaron <bitaron90@gmail.com>
2017-07-10 16:15:16 +0600

    Modify: Add test for FormEntityConverter

--- opensrp-core/src/test/java/org/opensrp/service/formSubmission/FormEntityConverterTest.java
+++ opensrp-core/src/test/java/org/opensrp/service/formSubmission/FormEntityConverterTest.java
@@ -2,18 +2,17 @@ package org.opensrp.service.formSubmission;
 
 
 import com.google.gson.Gson;
-import nl.jqno.equalsverifier.EqualsVerifier;
 import org.joda.time.DateTime;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.BDDMockito;
 import org.opensrp.BaseIntegrationTest;
-import org.opensrp.domain.*;
 import org.opensrp.common.FormEntityConstants;
+import org.opensrp.domain.Address;
+import org.opensrp.domain.Client;
+import org.opensrp.domain.Event;
+import org.opensrp.domain.Obs;
 import org.opensrp.form.domain.FormSubmission;
 import org.opensrp.form.service.FormAttributeParser;
 import org.opensrp.form.service.FormSubmissionMap;
-import org.powermock.api.mockito.PowerMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.xml.sax.SAXException;
 
@@ -124,21 +123,73 @@ public class FormEntityConverterTest extends BaseIntegrationTest {
         values.add("2015-05-07");
 
         Obs obs1 = new Obs("concept", "DateTime",
-                "160753AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",null ,values ,
-                null,  "FWNHREGDATE");
+                "160753AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", null, values,
+                null, "FWNHREGDATE");
         values.clear();
         values.add(2);
         Obs obs2 = new Obs("concept", "Integer",
                 "5611AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", null, values,
-                null,"FWNHHMBRNUM");
+                null, "FWNHHMBRNUM");
         expectedEvent.addObs(obs1);
         expectedEvent.addObs(obs2);
 
         Event event = formEntityConverter.getEventFromFormSubmission(fsubmission);
 
-        System.out.println(expectedEvent.equals(event));
-
         assertEvents(expectedEvent, event);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testExceptionWhileCreatingEventFromFromSubmission() {
+        FormSubmission fsubmission = new FormSubmission();
+        formEntityConverter.getEventFromFormSubmission(fsubmission);
+    }
+
+    @Test
+    public void testGetFieldName() throws IOException, XPathExpressionException, SAXException, ParserConfigurationException {
+        String expected = "FWHOHGENDER";
+        FormSubmission fsubmission = getFormSubmissionFor("new_household_registration", 1);
+        FormSubmissionMap formSubmissionMap = formAttributeParser.createFormSubmissionMap(fsubmission);
+        String actual = formEntityConverter.getFieldName(FormEntityConstants.Person.gender, formSubmissionMap);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetFieldNameInSubForm() throws IOException, XPathExpressionException, SAXException, ParserConfigurationException {
+        String expected = "FWGENDER";
+        FormSubmission fsubmission = getFormSubmissionFor("new_household_registration", 1);
+        FormSubmissionMap formSubmissionMap = formAttributeParser.createFormSubmissionMap(fsubmission);
+        String actual =
+                formEntityConverter.getFieldName(FormEntityConstants.Person.gender, formSubmissionMap.subforms().get(0));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInvalidFieldNameInForm() throws IOException, XPathExpressionException, SAXException, ParserConfigurationException {
+        FormSubmission fsubmission = getFormSubmissionFor("new_household_registration", 1);
+        FormSubmissionMap formSubmissionMap = formAttributeParser.createFormSubmissionMap(fsubmission);
+        String actual =
+                formEntityConverter.getFieldName(FormEntityConstants.Encounter.encounter_date, formSubmissionMap.subforms().get(0));
+        assertNull(actual);
+    }
+
+    @Test
+    public void testGetFieldNameUsingEntity() throws IOException, XPathExpressionException, SAXException, ParserConfigurationException {
+        String expected = "FWNHHMBRNUM";
+        FormSubmission fsubmission = getFormSubmissionFor("new_household_registration", 1);
+        FormSubmissionMap formSubmissionMap = formAttributeParser.createFormSubmissionMap(fsubmission);
+        String actual = formEntityConverter.getFieldName("concept","5611AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", null, formSubmissionMap);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetFieldNameUsingEnitityWithInvalidData() throws IOException, XPathExpressionException, SAXException, ParserConfigurationException {
+        FormSubmission fsubmission = getFormSubmissionFor("new_household_registration", 1);
+        FormSubmissionMap formSubmissionMap = formAttributeParser.createFormSubmissionMap(fsubmission);
+        String actual = formEntityConverter.getFieldName("concept","5611AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", null, formSubmissionMap.subforms().get(0));
+        assertNull(actual);
     }
 
     private void assertEvents(Event expected, Event actual) {
