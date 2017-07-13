d866dd35372833271e8ccff6c4ef750704662d5b
bitaron <bitaron90@gmail.com>
2017-07-13 16:56:14 +0600

    Modify: Add test.

---
+++ opensrp-core/src/test/java/org/opensrp/domain/BaseEntityTest.java
@@ -0,0 +1,32 @@
+package org.opensrp.domain;
+
+
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.Test;
+
+public class BaseEntityTest {
+
+    @Test
+    public void testGetterAndSetter() {
+        Validator validator = ValidatorBuilder.create()
+                .with(new SetterTester())
+                .with(new GetterTester())
+                .build();
+
+        validator.validate(PojoClassFactory.getPojoClass(BaseEntity.class));
+    }
+
+    @Test
+    public void testEqualAndHashcodeContract() {
+        EqualsVerifier.forClass(AppStateToken.class)
+                .suppress(Warning.NONFINAL_FIELDS)
+                .withRedefinedSuperclass()
+                .verify();
+    }
+}
