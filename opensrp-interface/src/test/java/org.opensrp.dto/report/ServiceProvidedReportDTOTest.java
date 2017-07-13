38aa2a96362745c57ba0afe9b21b882d52face63
real@mpower-social.com <real@mpower-social.com>
2017-07-13 15:52:04 +0600

    Added test code for ServiceProvidedReportDTO.java made equals() and hashCode() final

---
+++ opensrp-interface/src/test/java/org.opensrp.dto/report/ServiceProvidedReportDTOTest.java
@@ -0,0 +1,45 @@
+package org.opensrp.dto.report;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.construct.InstanceFactory;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.affirm.Affirm;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+import org.opensrp.dto.Utils.PojoTestUtils;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by real on 13/07/17.
+ */
+public class ServiceProvidedReportDTOTest {
+    @Test
+    public void testAccesors_shouldAccessProperField() {
+        PojoTestUtils.validateAccessors(ServiceProvidedReportDTO.class);
+    }
+
+    @Test
+    public void testEqualsAndHashCode() {
+        EqualsVerifier.forClass(ServiceProvidedReportDTO.class).suppress(Warning.NONFINAL_FIELDS).verify();
+    }
+
+    @Test
+    public void testConstructorAndwithSettersAndtoString() {
+        ServiceProvidedReportDTO serviceProvidedReportDTO, serviceProvidedReportDTO2;
+        serviceProvidedReportDTO = new ServiceProvidedReportDTO();
+        serviceProvidedReportDTO.withId(3);
+        serviceProvidedReportDTO.withDate(new LocalDate());
+        serviceProvidedReportDTO.withNRHMReportMonth(7);
+        serviceProvidedReportDTO.withNRHMReportYear(2017);
+        assertEquals((Integer) 3, serviceProvidedReportDTO.getId());
+        assertNotSame((Integer) 8, serviceProvidedReportDTO.getNrhm_report_month());
+
+        serviceProvidedReportDTO2 = new ServiceProvidedReportDTO(1, "", "", "", new LocalDate(), "", "", "", "", "", "");
+        assertTrue(serviceProvidedReportDTO2.toString().contains("id=1"));
+        assertFalse(serviceProvidedReportDTO2.toString().contains("village=gangapur"));
+    }
+
+}
