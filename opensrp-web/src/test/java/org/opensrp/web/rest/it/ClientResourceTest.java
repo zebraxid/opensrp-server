7a59a998f9e2375324a9a1afff2eb920fd2a6893
bitaron <bitaron90@gmail.com>
2017-07-20 17:28:26 +0600

    Modify: Add `MockMvc` class to test.
    Upgrade spring version from 3.1.0 to 3.1.1. Because `MockMvc` uses `SessionFlashMapManager` which is introduced in version 3.1.1.

--- opensrp-web/src/test/java/org/opensrp/web/rest/it/ClientResourceTest.java
+++ opensrp-web/src/test/java/org/opensrp/web/rest/it/ClientResourceTest.java
@@ -1,63 +1,65 @@
 package org.opensrp.web.rest.it;
 
-import static org.opensrp.common.AllConstants.BaseEntity.ADDRESS_TYPE;
-import static org.opensrp.common.AllConstants.BaseEntity.CITY_VILLAGE;
-import static org.opensrp.common.AllConstants.BaseEntity.COUNTRY;
-import static org.opensrp.common.AllConstants.BaseEntity.COUNTY_DISTRICT;
-import static org.opensrp.common.AllConstants.BaseEntity.STATE_PROVINCE;
-import static org.opensrp.common.AllConstants.BaseEntity.SUB_DISTRICT;
-import static org.opensrp.common.AllConstants.BaseEntity.SUB_TOWN;
-import static org.opensrp.common.AllConstants.BaseEntity.TOWN;
-import static org.opensrp.common.AllConstants.Client.BIRTH_DATE;
-import static org.opensrp.common.AllConstants.Client.DEATH_DATE;
-import static org.opensrp.common.AllConstants.Client.GENDER;
-import static org.opensrp.web.rest.RestUtils.getDateFilter;
-import static org.opensrp.web.rest.RestUtils.getStringFilter;
-
-import org.joda.time.DateTime;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.opensrp.domain.Client;
 import org.opensrp.service.ClientService;
 import org.opensrp.web.rest.ClientResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.server.MockMvc;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring/applicationContext-opensrp-web.xml")
-public class ClientResourceTest {
+import static org.opensrp.common.AllConstants.BaseEntity.SUB_TOWN;
+import static org.opensrp.common.AllConstants.BaseEntity.TOWN;
+import static org.opensrp.common.AllConstants.Client.BIRTH_DATE;
+import static org.opensrp.common.AllConstants.Client.GENDER;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.view;
 
-	private static final String BASE_URL = "http://localhost:8080/opensrp-web/client";  
-	private RestTemplate restTemplate;  
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = TestWebContextLoader.class, locations = {"classpath:spring/applicationContext-opensrp-web.xml"})
+public class ClientResourceTest {
+	@Autowired
+	private WebApplicationContext wac;
+
+	private static final String BASE_URL = "http://localhost:8080/opensrp-web/client";
+
+	private RestTemplate restTemplate;
 
 	MockHttpServletRequest req;
+
+	MockMvc mockMvc;
+
 
 	@Mock
 	private MockHttpServletResponse resp = new MockHttpServletResponse();
 
 	@Autowired
 	private ClientService cs;
+
 	@Autowired
 	private ClientResource cr;
-	
+
 	@Test
 	public void testClientSearch() throws Exception {
-		  MockHttpServletRequest mockRequest = new MockHttpServletRequest();
-		  //  mockRequest.setContentType(MediaType.APPLICATION_JSON.toString());
-		  mockRequest.setMethod("GET");
-		  mockRequest.setRequestURI("/rest/client/search");
-		  mockRequest.setAttribute(HandlerMapping.class.getName() + ".introspectTypeLevelMapping", true);
+		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+		//  mockRequest.setContentType(MediaType.APPLICATION_JSON.toString());
+		mockRequest.setMethod("GET");
+		mockRequest.setRequestURI("/rest/client/search");
+		mockRequest.setAttribute(HandlerMapping.class.getName() + ".introspectTypeLevelMapping", true);
 		mockRequest.addParameter("name", "firstName100");
 		mockRequest.addParameter(GENDER, "MALE");
 		mockRequest.addParameter(BIRTH_DATE, "2016-02-01:2016-03-21");
@@ -70,16 +72,25 @@ public class ClientResourceTest {
 		//mockRequest.addParameter(SUB_DISTRICT, request);
 		mockRequest.addParameter(TOWN, "Korangi");
 		mockRequest.addParameter(SUB_TOWN, "UC0");
-	
-		  AnnotationMethodHandlerAdapter handlerAdapter = new AnnotationMethodHandlerAdapter();
-		  HttpMessageConverter[] messageConverters = {new MappingJacksonHttpMessageConverter()};
-		  handlerAdapter.setMessageConverters(messageConverters);
-		
-		  MockHttpServletResponse mockResponse = new MockHttpServletResponse();
-		  handlerAdapter.handle(mockRequest, mockResponse, cr);
-		
-		  String actual = mockResponse.getContentAsString();
-		  System.out.println(actual);
+
+		AnnotationMethodHandlerAdapter handlerAdapter = new AnnotationMethodHandlerAdapter();
+		HttpMessageConverter[] messageConverters = { new MappingJacksonHttpMessageConverter() };
+		handlerAdapter.setMessageConverters(messageConverters);
+
+		MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+		handlerAdapter.handle(mockRequest, mockResponse, cr);
+
+		String actual = mockResponse.getContentAsString();
+		System.out.println(actual);
 
 	}
+
+	@Test
+	public void test() throws Exception {
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac)
+				.build();
+		this.mockMvc.perform(get("/").accept(MediaType.TEXT_HTML))
+				.andExpect(status().isOk());
+	}
+
 }
