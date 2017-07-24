470c928b504f385d1a8375cdd3b4b93ab138901c
bitaron <bitaron90@gmail.com>
2017-07-24 18:29:51 +0600

    Modify: Add test for `ClientResource`.

--- opensrp-web/src/test/java/org/opensrp/web/rest/it/ClientResourceTest.java
+++ opensrp-web/src/test/java/org/opensrp/web/rest/it/ClientResourceTest.java
@@ -27,7 +27,9 @@ import org.springframework.test.web.server.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+import org.springframework.web.util.NestedServletException;
 
+import java.io.IOException;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -39,6 +41,7 @@ import static org.opensrp.common.AllConstants.Client.BIRTH_DATE;
 import static org.opensrp.common.AllConstants.Client.GENDER;
 import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.server.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.server.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
 
@@ -77,7 +80,7 @@ public class ClientResourceTest {
 
 	@After
 	public void tearDown() {
-		//allClients.removeAll();
+		allClients.removeAll();
 	}
 
 	@Test
@@ -152,16 +155,97 @@ public class ClientResourceTest {
 		Client expectedClient = new Client("1").withFirstName("first").withGender("male")
 				.withBirthdate(new DateTime(0l, DateTimeZone.UTC), false);
 		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
-		MvcResult mvcResult = this.mockMvc.perform(
+		this.mockMvc.perform(
 				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedClient))
-						.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print()).andReturn();
-		String responseString = mvcResult.getResponse().getContentAsString();
-		System.out.println(responseString);
+						.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 		List<Client> allClientsInDb = allClients.getAll();
 		Client actualClient = allClientsInDb.get(0);
 		actualClient.setDateCreated(null); //So We don't need to mock DateTimeUtil.now()
 		assertEquals(1, allClientsInDb.size());
 		assertEquals(expectedClient, actualClient);
+	}
+
+	@Test(expected = NestedServletException.class)
+	public void shouldNotCreateClientWithOutBaseEntityId() throws Exception {
+		Client expectedClient = new Client("1").withGender("male").withBirthdate(new DateTime(0l, DateTimeZone.UTC), false);
+		expectedClient.setBaseEntityId(null);
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedClient))
+						.accept(MediaType.APPLICATION_JSON));
+		List<Client> allClientsInDb = allClients.getAll();
+		assertEquals(0, allClientsInDb.size());
+
+		expectedClient = new Client("1").withFirstName("first").withBirthdate(new DateTime(0l), false);
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedClient))
+						.accept(MediaType.APPLICATION_JSON));
+		allClientsInDb = allClients.getAll();
+		assertEquals(0, allClientsInDb.size());
+
+	}
+
+	@Test(expected = NestedServletException.class)
+	public void shouldNotCreateClientWithOutFirstName() throws Exception {
+		Client expectedClient = new Client("1").withGender("male").withBirthdate(new DateTime(0l, DateTimeZone.UTC), false);
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedClient))
+						.accept(MediaType.APPLICATION_JSON));
+		List<Client> allClientsInDb = allClients.getAll();
+		assertEquals(0, allClientsInDb.size());
+
+		expectedClient = new Client("1").withFirstName("first").withBirthdate(new DateTime(0l), false);
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedClient))
+						.accept(MediaType.APPLICATION_JSON));
+		allClientsInDb = allClients.getAll();
+		assertEquals(0, allClientsInDb.size());
+
+	}
+
+	@Test(expected = NestedServletException.class)
+	public void shouldNotCreateClientWithOutGender() throws Exception {
+		Client expectedClient = new Client("1").withFirstName("first").withBirthdate(new DateTime(0l), false);
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedClient))
+						.accept(MediaType.APPLICATION_JSON));
+		List<Client> allClientsInDb = allClients.getAll();
+		assertEquals(0, allClientsInDb.size());
+
+	}
+
+	@Test(expected = NestedServletException.class)
+	public void shouldNotCreateClientWithOutBirthDate() throws Exception {
+		Client expectedClient = new Client("1").withFirstName("first").withGender("male");
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedClient))
+						.accept(MediaType.APPLICATION_JSON));
+		List<Client> allClientsInDb = allClients.getAll();
+		assertEquals(0, allClientsInDb.size());
+
+	}
+
+	@Test
+	public void shouldUpdateExistingClient() throws Exception {
+		Client expectedClient = new Client("1").withFirstName("first").withGender("male")
+				.withBirthdate(new DateTime(0l, DateTimeZone.UTC), false);
+		createClient(asList(expectedClient));
+
+		expectedClient.setDeathdate(new DateTime(2l, DateTimeZone.UTC));
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL + "1").contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedClient))
+						.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+
+		List<Client> allClientsInDb = allClients.getAll();
+		Client actualClient = allClientsInDb.get(0);
+		actualClient.setDateEdited(null); //So We don't need to mock DateTimeUtil.now()
+		assertEquals(1, allClientsInDb.size());
+		assertEquals(expectedClient, actualClient);
+
 	}
 
 	private void createClient(List<Client> allClient) {
