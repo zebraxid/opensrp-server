ee552752ce16d739b6a0cb698ff6b91f7c14652d
bitaron <bitaron90@gmail.com>
2017-08-06 14:45:49 +0600

    Modify: Add test for `EventResource`.

--- opensrp-web/src/test/java/org/opensrp/web/rest/it/EventResourceTest.java
+++ opensrp-web/src/test/java/org/opensrp/web/rest/it/EventResourceTest.java
@@ -20,6 +20,7 @@ import org.springframework.test.web.server.MockMvc;
 import org.springframework.test.web.server.MvcResult;
 import org.springframework.test.web.server.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -32,6 +33,7 @@ import static org.opensrp.common.AllConstants.BaseEntity.BASE_ENTITY_ID;
 import static org.opensrp.common.AllConstants.Event.EVENT_TYPE;
 import static org.opensrp.common.AllConstants.Event.PROVIDER_ID;
 import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.server.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.server.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
 
@@ -39,6 +41,7 @@ import static org.springframework.test.web.server.result.MockMvcResultMatchers.s
 @ContextConfiguration(loader = TestWebContextLoader.class, locations = {
 		"classpath:spring/applicationContext-opensrp-web.xml" })
 public class EventResourceTest {
+
 	private final static String BASE_URL = "/rest/event/";
 
 	@Autowired
@@ -56,7 +59,6 @@ public class EventResourceTest {
 	private EventResource eventResource;
 
 	ObjectMapper mapper = new ObjectMapper();
-	
 
 	@Before
 	public void setUp() {
@@ -65,9 +67,8 @@ public class EventResourceTest {
 
 	@After
 	public void tearDown() {
-		//allEvents.removeAll();
+		allEvents.removeAll();
 	}
-
 
 	@Test
 	public void testRequiredProperties() {
@@ -77,11 +78,10 @@ public class EventResourceTest {
 		assertTrue(requiredProperties.contains(BASE_ENTITY_ID));
 	}
 
-
 	@Test
-	public void shouldFindClientById() throws Exception {
-		Event expectedEvent = new Event("1", "eventType",
-				new DateTime(0l, DateTimeZone.UTC), "entityType", "providerId", "locationId", "formSubmissionId");
+	public void shouldFindEventById() throws Exception {
+		Event expectedEvent = new Event("1", "eventType", new DateTime(0l, DateTimeZone.UTC), "entityType", "providerId",
+				"locationId", "formSubmissionId");
 		Map<String, String> identifiers = new HashMap<>();
 		identifiers.put("key", "value");
 		expectedEvent.setIdentifiers(identifiers);
@@ -100,16 +100,89 @@ public class EventResourceTest {
 	}
 
 	@Test
-	public void shouldNotFindClient() throws Exception {
+	public void shouldNotFindEvnet() throws Exception {
 		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
-		MvcResult mvcResult = this.mockMvc.perform(get(BASE_URL + "1").accept(MediaType.APPLICATION_JSON))
+		MvcResult mvcResult = this.mockMvc.perform(get(BASE_URL + "value").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk()).andDo(print()).andReturn();
 		String responseString = mvcResult.getResponse().getContentAsString();
 		assertTrue(responseString.isEmpty());
 	}
 
+	@Test
+	public void shouldCreateEventWithAllRequiredProperties() throws Exception {
+		Event expectedEvent = new Event("1", "eventType", new DateTime(0l, DateTimeZone.UTC), "entityType", "providerId",
+				"locationId", "formSubmissionId");
+		Map<String, String> identifiers = new HashMap<>();
+		identifiers.put("key", "value");
+		expectedEvent.setIdentifiers(identifiers);
+
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedEvent))
+						.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+		List<Event> allEventsInDB = allEvents.getAll();
+		Event actualEvent = allEventsInDB.get(0);
+		actualEvent.setDateCreated(null); //So We don't need to mock DateTimeUtil.now()
+		assertEquals(1, allEventsInDB.size());
+		assertEquals(expectedEvent, actualEvent);
+	}
+
+	@Test(expected = NestedServletException.class)
+	public void shouldNotCreateEventWithOutProviderId() throws Exception {
+		Event expectedEvent = new Event("1", "eventType", new DateTime(0l, DateTimeZone.UTC), "entityType", "providerId",
+				"locationId", "formSubmissionId");
+		Map<String, String> identifiers = new HashMap<>();
+		identifiers.put("key", "value");
+		expectedEvent.setIdentifiers(identifiers);
+		expectedEvent.setProviderId(null);
+
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedEvent))
+						.accept(MediaType.APPLICATION_JSON));
+
+		List<Event> allEventsInDb = allEvents.getAll();
+		assertEquals(0, allEventsInDb.size());
+	}
+
+	@Test(expected = NestedServletException.class)
+	public void shouldNotCreateEventWithOutEventType() throws Exception {
+		Event expectedEvent = new Event("1", "eventType", new DateTime(0l, DateTimeZone.UTC), "entityType", "providerId",
+				"locationId", "formSubmissionId");
+		Map<String, String> identifiers = new HashMap<>();
+		identifiers.put("key", "value");
+		expectedEvent.setIdentifiers(identifiers);
+		expectedEvent.setEventType(null);
+
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedEvent))
+						.accept(MediaType.APPLICATION_JSON));
+
+		List<Event> allEventsInDb = allEvents.getAll();
+		assertEquals(0, allEventsInDb.size());
+	}
+
+	@Test(expected = NestedServletException.class)
+	public void shouldNotCreateEventWithOutBaseEntityId() throws Exception {
+		Event expectedEvent = new Event("1", "eventType", new DateTime(0l, DateTimeZone.UTC), "entityType", "providerId",
+				"locationId", "formSubmissionId");
+		Map<String, String> identifiers = new HashMap<>();
+		identifiers.put("key", "value");
+		expectedEvent.setIdentifiers(identifiers);
+		expectedEvent.setBaseEntityId(null);
+
+		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
+		this.mockMvc.perform(
+				post(BASE_URL).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsBytes(expectedEvent))
+						.accept(MediaType.APPLICATION_JSON));
+
+		List<Event> allEventsInDb = allEvents.getAll();
+		assertEquals(0, allEventsInDb.size());
+	}
+
 	private void createEvent(List<Event> events) {
-		for(Event event : events) {
+		for (Event event : events) {
 			allEvents.add(event);
 		}
 	}
