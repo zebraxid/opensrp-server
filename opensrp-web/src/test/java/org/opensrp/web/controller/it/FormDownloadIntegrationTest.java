b5abd4774dec64618b571f2f6569a6eb4c3ed2b5
bitaron <bitaron90@gmail.com>
2017-08-17 16:12:31 +0600

    Modify: Move fetchting forms as zip to `TestResourceLodder`.

--- opensrp-web/src/test/java/org/opensrp/web/controller/it/FormDownloadIntegrationTest.java
+++ opensrp-web/src/test/java/org/opensrp/web/controller/it/FormDownloadIntegrationTest.java
@@ -1,42 +1,20 @@
 package org.opensrp.web.controller.it;
 
 import org.codehaus.jackson.JsonNode;
-import org.junit.Before;
 import org.junit.Test;
 import org.opensrp.web.rest.it.BaseResourceTest;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.server.MvcResult;
-import org.springframework.test.web.server.setup.MockMvcBuilders;
+import org.opensrp.web.utils.TestResourceLoader;
 import org.springframework.web.util.NestedServletException;
 
-import java.io.*;
-import java.util.Properties;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.io.IOException;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.server.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.server.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
 
 public class FormDownloadIntegrationTest extends BaseResourceTest {
 
-	String BASE_URL = "/form/";
-
-	String formToDownload;
-
-/*
-	@Before
-	public void setUp() {
-		this.mockMvc = MockMvcBuilders.webApplicationContextSetup(this.wac).build();
-	}
-*/
+	private String BASE_URL = "/form/";
 
 	@Test
 	public void shouldFetchAllAvailableVersion() throws Exception {
@@ -52,7 +30,7 @@ public class FormDownloadIntegrationTest extends BaseResourceTest {
 		String url = "form-files";
 
 		byte[] responseZipFile = getCallAsByeArray(BASE_URL + url, "formDirName=child_followup", status().isOk());
-		byte[] expectedZipFile = getFormDirectoryAsZip("child_followup");
+		byte[] expectedZipFile = new TestResourceLoader().getFormDirectoryAsZip("child_followup");
 
 		assertArrayEquals(expectedZipFile, responseZipFile);
 	}
@@ -63,55 +41,14 @@ public class FormDownloadIntegrationTest extends BaseResourceTest {
 		getCallAsByeArray(BASE_URL + url, "formDirName=invalid", status().isOk());
 	}
 
-	private byte[] getFormDirectoryAsZip(String directoryName) throws IOException {
-		Resource resource = new ClassPathResource("/opensrp.properties");
-		Properties props = PropertiesLoaderUtils.loadProperties(resource);
-		String formDirPath = props.getProperty("form.directory.name");
-		formToDownload = props.getProperty("form.download.files").replace(" ", "");
-		ResourceLoader loader = new DefaultResourceLoader();
-		return zipFiles(new File(loader.getResource(formDirPath).getURI().getPath() + "/" + directoryName));
-
-	}
-
-	private byte[] zipFiles(File directory) throws IOException {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ZipOutputStream zos = new ZipOutputStream(baos);
-		byte bytes[] = new byte[2048];
-		String[] fl = directory.list();
-		for (String fileName : fl) {
-			if (formToDownload.matches("(.+,)?" + fileName + "(,.+)?$")) {
-
-				FileInputStream fis = new FileInputStream(directory.getPath() + "/" + fileName);
-				BufferedInputStream bis = new BufferedInputStream(fis);
-
-				zos.putNextEntry(new ZipEntry(fileName));
-
-				int bytesRead;
-				while ((bytesRead = bis.read(bytes)) != -1) {
-					zos.write(bytes, 0, bytesRead);
-				}
-				zos.closeEntry();
-				bis.close();
-				fis.close();
-			}
-		}
-
-		zos.flush();
-		baos.flush();
-		zos.close();
-		baos.close();
-
-		return baos.toByteArray();
-	}
-
 	/**
 	 * Change the return type if any form name, directory or version changes.
 	 *
-	 * @return
+	 * @return JsonNode
 	 * @throws IOException
 	 */
 	private JsonNode currentFormVersions() throws IOException {
-		String currentFormVersionList = "{\"formVersions\" : [{\"formName\": \"Offsite_Child_Vaccination_Followup\", \"formDirName\": \"offsite_child_followup\", \"formDataDefinitionVersion\": \"1\"},{\"formName\": \"Woman_TT_Followup_Form\", \"formDirName\": \"woman_followup\", \"formDataDefinitionVersion\": \"1\"},{\"formName\": \"Child_Vaccination_Followup\", \"formDirName\": \"child_followup\", \"formDataDefinitionVersion\": \"1\"},{\"formName\": \"Child_Vaccination_Enrollment\", \"formDirName\": \"child_enrollment\", \"formDataDefinitionVersion\": \"1\"},{\"formName\": \"Vaccine_Stock_Position\", \"formDirName\": \"vaccine_stock_position\", \"formDataDefinitionVersion\": \"1\"},{\"formName\": \"Woman_TT_Enrollment_Form\", \"formDirName\": \"woman_enrollment\", \"formDataDefinitionVersion\": \"1\"},{\"formName\": \"Offsite_Woman_Followup_Form\", \"formDirName\": \"offsite_woman_followup\", \"formDataDefinitionVersion\": \"1\"}]}";
+		String currentFormVersionList = "{\"formVersions\":[{\"formName\":\"Offsite_Child_Vaccination_Followup\",\"formDirName\":\"offsite_child_followup\",\"formDataDefinitionVersion\":\"1\"},{\"formName\":\"Woman_TT_Followup_Form\",\"formDirName\":\"woman_followup\",\"formDataDefinitionVersion\":\"1\"},{\"formName\":\"Child_Vaccination_Followup\",\"formDirName\":\"child_followup\",\"formDataDefinitionVersion\":\"1\"},{\"formName\":\"Child_Vaccination_Enrollment\",\"formDirName\":\"child_enrollment\",\"formDataDefinitionVersion\":\"1\"},{\"formName\":\"Vaccine_Stock_Position\",\"formDirName\":\"vaccine_stock_position\",\"formDataDefinitionVersion\":\"1\"},{\"formName\":\"FWNewH\",\"formDirName\":\"new_household_registration\",\"formDataDefinitionVersion\":\"1\"},{\"formName\":\"Woman_TT_Enrollment_Form\",\"formDirName\":\"woman_enrollment\",\"formDataDefinitionVersion\":\"1\"},{\"formName\":\"Offsite_Woman_Followup_Form\",\"formDirName\":\"offsite_woman_followup\",\"formDataDefinitionVersion\":\"1\"}]}";
 		return mapper.readTree(currentFormVersionList);
 	}
 }
--- opensrp-web/src/test/java/org/opensrp/web/controller/it/FormSubmissionIntegrationTest.java
+++ opensrp-web/src/test/java/org/opensrp/web/controller/it/FormSubmissionIntegrationTest.java
@@ -1,15 +1,25 @@
 package org.opensrp.web.controller.it;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.opensrp.form.repository.AllFormSubmissions;
 import org.opensrp.web.rest.it.BaseResourceTest;
-import org.springframework.test.web.server.setup.MockMvcBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FormSubmissionIntegrationTest extends BaseResourceTest{
 
+	@Autowired
+	private AllFormSubmissions allFormSubmissions;
+
 	@Before
 	public void setUp() {
+		allFormSubmissions.removeAll();
+	}
 
+	@After
+	public void cleanUp() {
+		//allFormSubmissions.removeAll();
 	}
 
 	@Test
