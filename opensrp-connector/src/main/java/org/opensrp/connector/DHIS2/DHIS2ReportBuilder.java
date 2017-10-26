0901777b325fb202c666c4edd5a35b38e5657a3e
OUNAsha <asha@mpower-social.com>
2017-10-26 21:48:21 +0600

    updated for import data in DHIS2

--- opensrp-connector/src/main/java/org/opensrp/connector/DHIS2/DHIS2ReportBuilder.java
+++ opensrp-connector/src/main/java/org/opensrp/connector/DHIS2/DHIS2ReportBuilder.java
@@ -24,7 +24,7 @@ public class DHIS2ReportBuilder {
         this.orgUnitId = orgUnitId;
         this.completionDate = completionDate;
         this.period = period;
-        this.dhis2FormattedPeriod = "201709";
+        this.dhis2FormattedPeriod = "201710";
     }
 
     private String createDhis2FormattedPeriod() {
