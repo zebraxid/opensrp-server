7bb99d0f7a6a25074e1ab36c7882423e955f57d0
manutarus <manutarus@gmail.com>
2017-12-04 15:55:47 +0300

    saving images to s3

--- opensrp-core/src/main/java/org/opensrp/service/MultimediaService.java
+++ opensrp-core/src/main/java/org/opensrp/service/MultimediaService.java
@@ -1,8 +1,13 @@
 package org.opensrp.service;
 
-import java.io.File;
-import java.util.List;
-
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.opensrp.domain.Client;
 import org.opensrp.domain.Multimedia;
 import org.opensrp.dto.form.MultimediaDTO;
@@ -14,44 +19,62 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
+import java.util.List;
+
 @Service
 public class MultimediaService {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(MultimediaService.class.toString());
-	
+
 	public static final String IMAGES_DIR = "patient_images";
-	
+
 	private static final String VIDEOS_DIR = "videos";
-	
+
 	private final MultimediaRepository multimediaRepository;
-	
+
 	private final ClientService clientService;
-	
+
 	private String multimediaDirPath;
-	
+
 	@Value("#{opensrp['multimedia.directory.name']}")
 	String baseMultimediaDirPath;
-	
+
+	@Value("#{opensrp['aws_access_key_id']}")
+	String awsAccessKeyId;
+
+	@Value("#{opensrp['aws_secret_access_key']}")
+	String awsSecretAccessKey;
+
+	@Value("#{opensrp['aws_region']}")
+	String awsRegion;
+
+	@Value("#{opensrp['aws_bucket']}")
+	String awsBucket;
+
+	@Value("#{opensrp['aws_key_folder']}")
+	String awsKeyFolder;
+
 	@Autowired
 	public MultimediaService(MultimediaRepository multimediaRepository, ClientService clientService) {
 		this.multimediaRepository = multimediaRepository;
 		this.clientService = clientService;
 	}
-	
+
 	public String saveMultimediaFile(MultimediaDTO multimediaDTO, MultipartFile file) {
 
 		if (uploadFile(multimediaDTO, file)) {
 			try {
 				logger.info("Image path : " + multimediaDirPath);
-				
+
 				Multimedia multimediaFile = new Multimedia().withCaseId(multimediaDTO.getCaseId())
-				        .withProviderId(multimediaDTO.getProviderId()).withContentType(multimediaDTO.getContentType())
-				        .withFilePath(multimediaDTO.getFilePath()).withFileCategory(multimediaDTO.getFileCategory());
-				
+						.withProviderId(multimediaDTO.getProviderId()).withContentType(multimediaDTO.getContentType())
+						.withFilePath(multimediaDTO.getFilePath()).withFileCategory(multimediaDTO.getFileCategory());
+
 				multimediaRepository.add(multimediaFile);
 				Client client = clientService.getByBaseEntityId(multimediaDTO.getCaseId());
-				if (client !=null) {
-					if(client.getAttribute("Patient Image") != null) {
+				if (client != null) {
+					if (client.getAttribute("Patient Image") != null) {
 						client.removeAttribute("Patient Image");
 					}
 					client.addAttribute("Patient Image", multimediaDTO.getCaseId() + ".jpg");
@@ -64,59 +87,53 @@ public class MultimediaService {
 				e.getMessage();
 			}
 		}
-		
+
 		return "fail";
 	}
-	
+
 	public boolean uploadFile(MultimediaDTO multimediaDTO, MultipartFile multimediaFile) {
-		
+
 		// String baseMultimediaDirPath = System.getProperty("user.home");
-		
+
 		if (!multimediaFile.isEmpty()) {
 			try {
-				
+
 				multimediaDirPath = baseMultimediaDirPath + File.separator;
 				String fileExt = ".jpg";
 				switch (multimediaDTO.getContentType()) {
-					
+
 					case "application/octet-stream":
 						multimediaDirPath += VIDEOS_DIR;
 						fileExt = ".mp4";
 						break;
-					
+
 					case "image/jpeg":
 						multimediaDirPath += IMAGES_DIR;
 						fileExt = ".jpg";
 						break;
-					
+
 					case "image/gif":
 						multimediaDirPath += IMAGES_DIR;
 						fileExt = ".gif";
 						break;
-					
+
 					case "image/png":
 						multimediaDirPath += IMAGES_DIR;
 						fileExt = ".png";
 						break;
-					
+
 				}
 				new File(multimediaDirPath).mkdir();
 				String fileName = multimediaDirPath + File.separator + multimediaDTO.getCaseId() + fileExt;
 				multimediaDTO.withFilePath(fileName);
 				File multimediaDir = new File(fileName);
-				
+
 				multimediaFile.transferTo(multimediaDir);
-				
-				/*
-				 byte[] bytes = multimediaFile.getBytes();
-				 	
-				 BufferedOutputStream stream = new BufferedOutputStream(
-							new FileOutputStream(multimediaDirPath));
-					stream.write(bytes);
-					stream.close();*/
-				
+
+				uploadImageTos3(multimediaDir, awsAccessKeyId, awsSecretAccessKey, awsRegion,awsBucket, awsKeyFolder);
+
 				return true;
-				
+
 			}
 			catch (Exception e) {
 				logger.error("", e);
@@ -126,19 +143,43 @@ public class MultimediaService {
 			return false;
 		}
 	}
-	
-	private void makeMultimediaDir(String dirPath) {
-		File file = new File(dirPath);
-		if (!file.exists())
-			file.mkdirs();
-		
-	}
-	
+
 	public List<Multimedia> getMultimediaFiles(String providerId) {
 		return multimediaRepository.all(providerId);
 	}
-	
+
 	public Multimedia findByCaseId(String entityId) {
 		return multimediaRepository.findByCaseId(entityId);
+	}
+
+	public boolean uploadImageTos3(File imageFile, String awsAccessKeyId, String awsSecretAccessKey, String awsRegion,
+	                               String awsBucket, String awsKeyFolder) {
+
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(
+				new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKeyId, awsSecretAccessKey)))
+				.withRegion(awsRegion).build();
+		InputStream inputStream;
+		try {
+			byte[] md5 = DigestUtils.md5(new FileInputStream(imageFile));
+			inputStream = new FileInputStream(imageFile);
+			ObjectMetadata metadata = new ObjectMetadata();
+			metadata.setContentLength(imageFile.length());
+			metadata.setContentMD5(new String(Base64.encodeBase64(md5)));
+			PutObjectRequest request = new PutObjectRequest(awsBucket, awsKeyFolder + imageFile.getName(), inputStream,
+					metadata);
+			//			request.setCannedAcl(CannedAccessControlList.PublicRead);
+			//			PutObjectResult result = s3Client.putObject(request);
+			s3Client.putObject(request);
+			return true;
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
 	}
 }
--- opensrp-core/src/test/java/org/opensrp/MultimediaServiceTest.java
+++ opensrp-core/src/test/java/org/opensrp/MultimediaServiceTest.java
@@ -1,13 +1,5 @@
 package org.opensrp;
 
-import static org.mockito.MockitoAnnotations.initMocks;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -26,38 +18,62 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-applicationContext-opensrp.xml")
 public class MultimediaServiceTest {
-	
+
+	@Value("#{opensrp['aws_access_key_id']}")
+	String awsAccessKeyId;
+
+	@Value("#{opensrp['aws_secret_access_key']}")
+	String awsSecretAccessKey;
+
+	@Value("#{opensrp['aws_region']}")
+	String awsRegion;
+
+	@Value("#{opensrp['aws_bucket']}")
+	String awsBucket;
+
+	@Value("#{opensrp['aws_key_folder']}")
+	String awsKeyFolder;
+
 	@Mock
 	private MultimediaService multimediaService;
-	
+
 	@Autowired
 	private MultimediaRepository multimediaRepository;
-	
+
 	@Autowired
 	private ClientService clientService;
-	
+
 	@Autowired
 	@Value("#{opensrp['multimedia.directory.name']}")
 	private String multimediaDirPath;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		initMocks(this);
+
 		multimediaService = new MultimediaService(multimediaRepository, clientService);
 	}
-	
+
 	@Ignore
 	@Test
 	public void shouldSaveMultimediaFile() throws FileNotFoundException {
 		MultimediaDTO multimedia = new MultimediaDTO("1234567891", "opensrp", "image/jpeg", "", "nid");
 		String baseDirPath = System.getProperty("user.home");
 		FileInputStream fis = new FileInputStream(baseDirPath + File.separator + ".OpenSRP/multimedia/image.jpeg");
-		
+
 		MultipartFile multipartFile = null;
-		
+
 		try {
 			multipartFile = new MockMultipartFile("file", fis);
 		}
@@ -65,43 +81,51 @@ public class MultimediaServiceTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		String status = multimediaService.saveMultimediaFile(multimedia, multipartFile);
-		
+
 		Assert.assertEquals("success", status);
-		
+
 	}
-	
+
 	@Ignore
 	@Test
 	public void shouldGetMultimediaFiles() throws FileNotFoundException {
 		MultimediaDTO multimediaDTO = new MultimediaDTO("1234567890", "opensrp", "image/jpeg", "", "profile");
-		
+
 		Multimedia expectedMultimedia = new Multimedia().withCaseId(multimediaDTO.getCaseId())
-		        .withProviderId(multimediaDTO.getProviderId()).withContentType(multimediaDTO.getContentType())
-		        .withFilePath(multimediaDTO.getFilePath()).withFileCategory(multimediaDTO.getFileCategory());
-		
+				.withProviderId(multimediaDTO.getProviderId()).withContentType(multimediaDTO.getContentType())
+				.withFilePath(multimediaDTO.getFilePath()).withFileCategory(multimediaDTO.getFileCategory());
+
 		String baseDirPath = System.getProperty("user.home");
 		FileInputStream fis = new FileInputStream(baseDirPath + File.separator + ".OpenSRP/multimedia/image.jpeg");
 		MultipartFile multipartFile = null;
-		
+
 		try {
 			multipartFile = new MockMultipartFile("file", fis);
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		boolean status = multimediaService.uploadFile(multimediaDTO, multipartFile);
-		
+
 		if (status)
 			multimediaRepository.add(expectedMultimedia);
-		
+
 		List<Multimedia> multimediaFiles = multimediaService.getMultimediaFiles("opensrp");
-		
+
 		for (Multimedia actualMultimedia : multimediaFiles) {
 			if (actualMultimedia.getCaseId().equals(multimediaDTO.getCaseId()))
 				Assert.assertEquals(expectedMultimedia.getFilePath(), actualMultimedia.getFilePath());
 		}
+	}
+
+	@Test
+	public void shouldSaveImageTos3() throws Exception {
+		File fileImage = new File("/Users/ona/.OpenMRS/patient_images/7f58a6cf-51ec-4bb4-929e-5ffcd3b1fac3.jpg");
+		Assert.assertTrue(multimediaService
+				.uploadImageTos3(fileImage, awsAccessKeyId, awsSecretAccessKey, awsRegion, awsBucket, awsKeyFolder));
+
 	}
 }
