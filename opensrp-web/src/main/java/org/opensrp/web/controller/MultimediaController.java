37cc05e5a7bcfb373a565a2c10d2a3a43fd14e56
manutarus <manutarus@gmail.com>
2017-12-10 15:09:01 +0300

    s3 funtianlity

--- opensrp-web/src/main/java/org/opensrp/web/controller/MultimediaController.java
+++ opensrp-web/src/main/java/org/opensrp/web/controller/MultimediaController.java
@@ -26,7 +26,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
