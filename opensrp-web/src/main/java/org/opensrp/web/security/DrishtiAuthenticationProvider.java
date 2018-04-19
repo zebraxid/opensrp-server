a984a76afc89ff96b9c1e4975aacd1c698d0cede
Ephraim Muhia <emuhia@ona.io>
2018-04-19 15:28:15 +0300

    Added custom error messages
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-web/src/main/java/org/opensrp/web/security/DrishtiAuthenticationProvider.java
+++ opensrp-web/src/main/java/org/opensrp/web/security/DrishtiAuthenticationProvider.java
@@ -4,7 +4,6 @@ import static java.text.MessageFormat.format;
 
 import java.util.List;
 
-import org.json.JSONException;
 import org.opensrp.api.domain.User;
 import org.opensrp.connector.openmrs.service.OpenmrsUserService;
 import org.slf4j.Logger;
@@ -25,70 +24,85 @@ import ch.lambdaj.function.convert.Converter;
 
 @Component
 public class DrishtiAuthenticationProvider implements AuthenticationProvider {
-    private static Logger logger = LoggerFactory.getLogger(DrishtiAuthenticationProvider.class.toString());
-    public static final String USER_NOT_FOUND = "The username or password you entered is incorrect. Please enter the correct credentials.";
-    public static final String USER_NOT_ACTIVATED = "The user has been registered but not activated. Please contact your local administrator.";
-    public static final String INTERNAL_ERROR = "Failed to authenticate user due to internal server error.";
-
-    //private AllOpenSRPUsers allOpenSRPUsers;
-    private PasswordEncoder passwordEncoder;
-    private OpenmrsUserService openmrsUserService;
-
-
-    @Autowired
-    public DrishtiAuthenticationProvider(OpenmrsUserService openmrsUserService, @Qualifier("shaPasswordEncoder") PasswordEncoder passwordEncoder) {
-        this.openmrsUserService = openmrsUserService;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-    	User user = getDrishtiUser(authentication, authentication.getName());
-    	// get user after authentication
-        if (user == null) {
-            throw new BadCredentialsException(USER_NOT_FOUND);
-        }
-
-        if (user.getVoided() != null && user.getVoided()) {
-            throw new BadCredentialsException(USER_NOT_ACTIVATED);
-        }
-        
-        return new UsernamePasswordAuthenticationToken(authentication.getName(), authentication.getCredentials(), getRolesAsAuthorities(user));
-    }
-
-    @Override
-    public boolean supports(Class<?> authentication) {
-        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication)
-                && authentication.equals(UsernamePasswordAuthenticationToken.class);
-    }
-
-    private List<SimpleGrantedAuthority> getRolesAsAuthorities(User user) {
-        return Lambda.convert(user.getRoles(), new Converter<String, SimpleGrantedAuthority>() {
-            @Override
-            public SimpleGrantedAuthority convert(String role) {
-                return new SimpleGrantedAuthority("ROLE_OPENMRS");
-            }
-        });
-    }
-
-    
-    
-    public User getDrishtiUser(Authentication authentication, String username) {
-        User user = null;
-        try {
-        	if(openmrsUserService.authenticate(authentication.getName(), authentication.getCredentials().toString())){
-                boolean response = openmrsUserService.deleteSession(authentication.getName(),authentication.getCredentials().toString());
-			    user = openmrsUserService.getUser(username);
-			    if(!response){
-		            logger.error(format("{0}. Exception: {1}", INTERNAL_ERROR, "Unable to clear session"));
-
-			    }
+	
+	private static Logger logger = LoggerFactory.getLogger(DrishtiAuthenticationProvider.class.toString());
+	
+	public static final String INVALID_CREDENTIALS = "The username or password you entered is incorrect. Please enter the correct credentials.";
+	
+	public static final String USER_NOT_FOUND = "The user has not been found. Please try again later.";
+	
+	public static final String USER_NOT_ACTIVATED = "The user has been registered but not activated. Please contact your local administrator.";
+	
+	public static final String INTERNAL_ERROR = "Failed to authenticate user due to internal server error. Please try again later.";
+	
+	//private AllOpenSRPUsers allOpenSRPUsers;
+	private PasswordEncoder passwordEncoder;
+	
+	private OpenmrsUserService openmrsUserService;
+	
+	@Autowired
+	public DrishtiAuthenticationProvider(OpenmrsUserService openmrsUserService,
+	    @Qualifier("shaPasswordEncoder") PasswordEncoder passwordEncoder) {
+		this.openmrsUserService = openmrsUserService;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		User user = getDrishtiUser(authentication, authentication.getName());
+		// get user after authentication
+		if (user == null) {
+			throw new BadCredentialsException(USER_NOT_FOUND);
+		}
+		
+		if (user.getVoided() != null && user.getVoided()) {
+			throw new BadCredentialsException(USER_NOT_ACTIVATED);
+		}
+		
+		return new UsernamePasswordAuthenticationToken(authentication.getName(), authentication.getCredentials(),
+		        getRolesAsAuthorities(user));
+	}
+	
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication)
+		        && authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+	
+	private List<SimpleGrantedAuthority> getRolesAsAuthorities(User user) {
+		return Lambda.convert(user.getRoles(), new Converter<String, SimpleGrantedAuthority>() {
+			
+			@Override
+			public SimpleGrantedAuthority convert(String role) {
+				return new SimpleGrantedAuthority("ROLE_OPENMRS");
 			}
-        } catch (Exception e) {
-            logger.error(format("{0}. Exception: {1}", INTERNAL_ERROR, e));
-        	e.printStackTrace();
-            throw new BadCredentialsException(INTERNAL_ERROR);
-        }
-        return user;
-    }
+		});
+	}
+	
+	public User getDrishtiUser(Authentication authentication, String username) {
+		User user = null;
+		try {
+			Boolean isAuthenticated = openmrsUserService.authenticate(authentication.getName(),
+			    authentication.getCredentials().toString());
+			if (isAuthenticated == null) {
+				throw new BadCredentialsException(INTERNAL_ERROR);
+			} else if (!isAuthenticated) {
+				throw new BadCredentialsException(INVALID_CREDENTIALS);
+			}
+			
+			boolean response = openmrsUserService.deleteSession(authentication.getName(),
+			    authentication.getCredentials().toString());
+			user = openmrsUserService.getUser(username);
+			if (!response) {
+				logger.error(format("{0}. Exception: {1}", INTERNAL_ERROR, "Unable to clear session"));
+				
+			}
+		}
+		catch (Exception e) {
+			logger.error(format("{0}. Exception: {1}", INTERNAL_ERROR, e));
+			e.printStackTrace();
+			throw new BadCredentialsException(INTERNAL_ERROR);
+		}
+		return user;
+	}
 }
