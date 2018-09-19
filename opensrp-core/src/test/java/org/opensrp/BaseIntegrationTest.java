ef904288ad04a8af75c3fb5a5369ea19206477f8
Manu Tarus <manutarus@gmail.com>
2018-09-19 12:08:37 +0300

    codacy fixes

--- opensrp-core/src/test/java/org/opensrp/BaseIntegrationTest.java
+++ opensrp-core/src/test/java/org/opensrp/BaseIntegrationTest.java
@@ -1,11 +1,12 @@
 package org.opensrp;
 
 import org.junit.Ignore;
+import org.junit.Test;
+
 
 public class BaseIntegrationTest  extends
 		SpringApplicationContextProvider{
-
-	@Ignore
+	@Ignore@Test
 	public void testOk(){
 	}
 
