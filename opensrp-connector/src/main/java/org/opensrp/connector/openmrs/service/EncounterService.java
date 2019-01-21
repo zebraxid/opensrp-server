2ab8c545953f82e7d3125cfcc302a58fac34e40d
nursat@mpower-social.com <nursat@mpower-social.com>
2019-01-21 11:24:55 +0600

    bahmni and opensrp-server data sync

--- opensrp-connector/src/main/java/org/opensrp/connector/openmrs/service/EncounterService.java
+++ opensrp-connector/src/main/java/org/opensrp/connector/openmrs/service/EncounterService.java
@@ -12,12 +12,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.opensrp.common.util.HttpResponse;
 import org.opensrp.common.util.HttpUtil;
+import org.opensrp.connector.openmrs.schedule.OpenmrsSyncerListener;
 import org.opensrp.domain.Client;
 import org.opensrp.domain.Event;
 import org.opensrp.domain.Obs;
 import org.opensrp.domain.User;
 import org.opensrp.service.ClientService;
 import org.opensrp.service.EventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +28,7 @@ import com.mysql.jdbc.StringUtils;
 
 @Service
 public class EncounterService extends OpenmrsService {
+	private static Logger logger = LoggerFactory.getLogger(EncounterService.class.toString());
 	
 	private static final String ENCOUNTER_URL = "ws/rest/v1/encounter";//"ws/rest/emrapi/encounter";
 	
@@ -122,69 +126,116 @@ public class EncounterService extends OpenmrsService {
 	}
 	
 	public JSONObject createEncounter(Event e) throws JSONException {
-		JSONObject pt = patientService.getPatientByIdentifier(e.getBaseEntityId());
-		JSONObject enc = new JSONObject();
-		
-		JSONObject pr = userService.getPersonByUser(e.getProviderId());
-		
-		enc.put("encounterDatetime", OPENMRS_DATE.format(e.getEventDate().toDate()));
-		// patient must be existing in OpenMRS before it submits an encounter. if it doesnot it would throw NPE
-		enc.put("patient", pt.getString("uuid"));
-		//TODO enc.put("patientUuid", pt.getString("uuid"));
-		enc.put("encounterType", e.getEventType());
-		//TODO enc.put("encounterTypeUuid", e.getEventType());
-		enc.put("location", e.getLocationId());
-		//enc.put("provider", pr.getString("uuid"));
-		
-		List<Obs> ol = e.getObs();
-		Map<String, JSONArray> p = new HashMap<>();
-		Map<String, JSONArray> pc = new HashMap<>();
-		
-		if (ol != null)
-			for (Obs obs : ol) {
-				if (!StringUtils.isEmptyOrWhitespaceOnly(obs.getFieldCode())
-				        && (obs.getFieldType() == null || obs.getFieldType().equalsIgnoreCase("concept"))) {//skipping empty obs and fields that don't have concepts
-					//if no parent simply make it root obs
-					if (StringUtils.isEmptyOrWhitespaceOnly(obs.getParentCode())) {
-						p.put(obs.getFieldCode(), convertObsToJson(obs));
-					} else {
-						//find parent obs if not found search and fill or create one
-						JSONArray parentObs = p.get(obs.getParentCode());
-						if (parentObs == null) {
-							p.put(obs.getParentCode(), convertObsToJson(getOrCreateParent(ol, obs)));
-						}
-						// find if any other exists with same parent if so add to the list otherwise create new list
-						JSONArray obl = pc.get(obs.getParentCode());
-						if (obl == null) {
-							obl = new JSONArray();
-						}
-						JSONArray addobs = convertObsToJson(obs);
-						for (int i = 0; i < addobs.length(); i++) {
-							obl.put(addobs.getJSONObject(i));
-						}
-						pc.put(obs.getParentCode(), obl);
-					}
-				}
-			}
-		
-		JSONArray obar = new JSONArray();
-		for (String ok : p.keySet()) {
-			for (int i = 0; i < p.get(ok).length(); i++) {
-				JSONObject obo = p.get(ok).getJSONObject(i);
-				
-				JSONArray cob = pc.get(ok);
-				if (cob != null && cob.length() > 0) {
-					obo.put("groupMembers", cob);
-				}
-				
-				obar.put(obo);
-			}
-		}
-		enc.put("obs", obar);
-		
-		HttpResponse op = HttpUtil.post(HttpUtil.removeEndingSlash(OPENMRS_BASE_URL) + "/" + ENCOUNTER_URL, "",
+		JSONObject enc = createEncounterJson(e);
+		//JSONObject enc2 = new JSONObject("{\"locationUuid\":\"7d76f621-63ce-4bbb-a715-176286f8de84\",\"patientUuid\":\"b0e47458-1bcb-42d4-88d7-f5aaaa2aa1af\",\"encounterUuid\":null,\"visitUuid\":null,\"providers\":[{\"uuid\":\"313c8507-9821-40e4-8a70-71a5c7693d72\"}],\"encounterDateTime\":null,\"extensions\":{\"mdrtbSpecimen\":[]},\"context\":{},\"visitType\":\"OPD\",\"bahmniDiagnoses\":[],\"orders\":[],\"drugOrders\":[],\"disposition\":null,\"observations\":[{\"concept\":{\"uuid\":\"f87fe2f4-de90-4c48-b07d-4050a5debf8c\",\"name\":\"\u09B8\u09CD\u09AC\u09BE\u09B8\u09CD\u09A5\u09CD\u09AF \u09B8\u09C7\u09AC\u09BE:\"},\"formNamespace\":\"Bahmni\",\"formFieldPath\":\"\u09B8\u09BE\u09A7\u09BE\u09B0\u09A8 \u09B0\u09C7\u09BE\u0997\u09C0\u09B0 \u09B8\u09C7\u09AC\u09BE \u09AB\u09B0\u09CD\u09AE.12/39-0\",\"voided\":false,\"value\":{\"uuid\":\"c2bb6edf-18cb-4c7f-ad91-7c8dd561a437\",\"name\":{\"display\":\"\u0989\u099A\u09CD\u099A \u09B0\u0995\u09CD\u09A4\u099A\u09BE\u09AA\",\"uuid\":\"38b6a8b8-e02f-41c7-813d-63df201562a6\",\"name\":\"\u0989\u099A\u09CD\u099A \u09B0\u0995\u09CD\u09A4\u099A\u09BE\u09AA\",\"locale\":\"en\",\"localePreferred\":true,\"conceptNameType\":null,\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/c2bb6edf-18cb-4c7f-ad91-7c8dd561a437/name/38b6a8b8-e02f-41c7-813d-63df201562a6\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/c2bb6edf-18cb-4c7f-ad91-7c8dd561a437/name/38b6a8b8-e02f-41c7-813d-63df201562a6?v=full\"}],\"resourceVersion\":\"1.9\"},\"names\":[{\"display\":\"\u0989\u099A\u09CD\u099A \u09B0\u0995\u09CD\u09A4\u099A\u09BE\u09AA\",\"uuid\":\"38b6a8b8-e02f-41c7-813d-63df201562a6\",\"name\":\"\u0989\u099A\u09CD\u099A \u09B0\u0995\u09CD\u09A4\u099A\u09BE\u09AA\",\"locale\":\"en\",\"localePreferred\":true,\"conceptNameType\":null,\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/c2bb6edf-18cb-4c7f-ad91-7c8dd561a437/name/38b6a8b8-e02f-41c7-813d-63df201562a6\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/c2bb6edf-18cb-4c7f-ad91-7c8dd561a437/name/38b6a8b8-e02f-41c7-813d-63df201562a6?v=full\"}],\"resourceVersion\":\"1.9\"},{\"display\":\"High Blood Pressure\",\"uuid\":\"98a2f125-8eac-4680-bd08-33572ff023d0\",\"name\":\"High Blood Pressure\",\"locale\":\"en\",\"localePreferred\":false,\"conceptNameType\":\"FULLY_SPECIFIED\",\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/c2bb6edf-18cb-4c7f-ad91-7c8dd561a437/name/98a2f125-8eac-4680-bd08-33572ff023d0\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/c2bb6edf-18cb-4c7f-ad91-7c8dd561a437/name/98a2f125-8eac-4680-bd08-33572ff023d0?v=full\"}],\"resourceVersion\":\"1.9\"}],\"displayString\":\"\u0989\u099A\u09CD\u099A \u09B0\u0995\u09CD\u09A4\u099A\u09BE\u09AA\",\"resourceVersion\":\"2.0\",\"translationKey\":\"\u0989\u099A\u09CD\u099A_\u09B0\u0995\u09CD\u09A4\u099A\u09BE\u09AA_39\"},\"inactive\":false,\"groupMembers\":[]},{\"concept\":{\"uuid\":\"9be72615-b914-4d35-ac80-d5953744a9d0\",\"name\":\"\u09B0\u09C7\u09AB\u09BE\u09B0\u09C7\u09B2:\"},\"formNamespace\":\"Bahmni\",\"formFieldPath\":\"\u09B8\u09BE\u09A7\u09BE\u09B0\u09A8 \u09B0\u09C7\u09BE\u0997\u09C0\u09B0 \u09B8\u09C7\u09AC\u09BE \u09AB\u09B0\u09CD\u09AE.12/40-0\",\"voided\":false,\"value\":{\"uuid\":\"1e3f1870-b252-4808-8edb-f86fad050ebd\",\"name\":{\"display\":\"\u09A1\u09BE\u09AF\u09BC\u09BE\u09AC\u09C7\u099F\u09BF\u09B8\",\"uuid\":\"befce65b-9e80-45ec-b8b7-05234cd5cb9c\",\"name\":\"\u09A1\u09BE\u09AF\u09BC\u09BE\u09AC\u09C7\u099F\u09BF\u09B8\",\"locale\":\"en\",\"localePreferred\":true,\"conceptNameType\":null,\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/1e3f1870-b252-4808-8edb-f86fad050ebd/name/befce65b-9e80-45ec-b8b7-05234cd5cb9c\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/1e3f1870-b252-4808-8edb-f86fad050ebd/name/befce65b-9e80-45ec-b8b7-05234cd5cb9c?v=full\"}],\"resourceVersion\":\"1.9\"},\"names\":[{\"display\":\"Diabetes\",\"uuid\":\"fdabcf86-7ac9-4122-96f7-9f84858228fd\",\"name\":\"Diabetes\",\"locale\":\"en\",\"localePreferred\":false,\"conceptNameType\":\"FULLY_SPECIFIED\",\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/1e3f1870-b252-4808-8edb-f86fad050ebd/name/fdabcf86-7ac9-4122-96f7-9f84858228fd\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/1e3f1870-b252-4808-8edb-f86fad050ebd/name/fdabcf86-7ac9-4122-96f7-9f84858228fd?v=full\"}],\"resourceVersion\":\"1.9\"},{\"display\":\"\u09A1\u09BE\u09AF\u09BC\u09BE\u09AC\u09C7\u099F\u09BF\u09B8\",\"uuid\":\"befce65b-9e80-45ec-b8b7-05234cd5cb9c\",\"name\":\"\u09A1\u09BE\u09AF\u09BC\u09BE\u09AC\u09C7\u099F\u09BF\u09B8\",\"locale\":\"en\",\"localePreferred\":true,\"conceptNameType\":null,\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/1e3f1870-b252-4808-8edb-f86fad050ebd/name/befce65b-9e80-45ec-b8b7-05234cd5cb9c\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/1e3f1870-b252-4808-8edb-f86fad050ebd/name/befce65b-9e80-45ec-b8b7-05234cd5cb9c?v=full\"}],\"resourceVersion\":\"1.9\"}],\"displayString\":\"\u09A1\u09BE\u09AF\u09BC\u09BE\u09AC\u09C7\u099F\u09BF\u09B8\",\"resourceVersion\":\"2.0\",\"translationKey\":\"\u09A1\u09BE\u09AF\u09BC\u09BE\u09AC\u09C7\u099F\u09BF\u09B8_40\"},\"inactive\":false,\"groupMembers\":[]},{\"concept\":{\"uuid\":\"514de0ad-14e3-4fc6-b4a3-a2683317ab53\",\"name\":\"\u09B8\u09CD\u09AC\u09BE\u09B8\u09CD\u09A5\u09CD\u09AF \u09B6\u09BF\u0995\u09CD\u09B7\u09BE:\"},\"formNamespace\":\"Bahmni\",\"formFieldPath\":\"\u09B8\u09BE\u09A7\u09BE\u09B0\u09A8 \u09B0\u09C7\u09BE\u0997\u09C0\u09B0 \u09B8\u09C7\u09AC\u09BE \u09AB\u09B0\u09CD\u09AE.12/41-0\",\"voided\":false,\"value\":{\"uuid\":\"0622f52f-0c95-41c1-ab5d-ee9bc335c839\",\"name\":{\"display\":\"\u09B8\u09AE\u09CD\u09AD\u09BE\u09AC\u09CD\u09AF \u09AF\u0995\u09CD\u09B7\u09CD\u09AE\u09BE\",\"uuid\":\"cc994816-e03d-4674-a455-f1087b88e934\",\"name\":\"\u09B8\u09AE\u09CD\u09AD\u09BE\u09AC\u09CD\u09AF \u09AF\u0995\u09CD\u09B7\u09CD\u09AE\u09BE\",\"locale\":\"en\",\"localePreferred\":true,\"conceptNameType\":null,\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/0622f52f-0c95-41c1-ab5d-ee9bc335c839/name/cc994816-e03d-4674-a455-f1087b88e934\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/0622f52f-0c95-41c1-ab5d-ee9bc335c839/name/cc994816-e03d-4674-a455-f1087b88e934?v=full\"}],\"resourceVersion\":\"1.9\"},\"names\":[{\"display\":\"Tuberculosis\",\"uuid\":\"9c02fdc7-0e51-4a1f-a530-586bfed811a9\",\"name\":\"Tuberculosis\",\"locale\":\"en\",\"localePreferred\":false,\"conceptNameType\":\"FULLY_SPECIFIED\",\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/0622f52f-0c95-41c1-ab5d-ee9bc335c839/name/9c02fdc7-0e51-4a1f-a530-586bfed811a9\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/0622f52f-0c95-41c1-ab5d-ee9bc335c839/name/9c02fdc7-0e51-4a1f-a530-586bfed811a9?v=full\"}],\"resourceVersion\":\"1.9\"},{\"display\":\"\u09B8\u09AE\u09CD\u09AD\u09BE\u09AC\u09CD\u09AF \u09AF\u0995\u09CD\u09B7\u09CD\u09AE\u09BE\",\"uuid\":\"cc994816-e03d-4674-a455-f1087b88e934\",\"name\":\"\u09B8\u09AE\u09CD\u09AD\u09BE\u09AC\u09CD\u09AF \u09AF\u0995\u09CD\u09B7\u09CD\u09AE\u09BE\",\"locale\":\"en\",\"localePreferred\":true,\"conceptNameType\":null,\"links\":[{\"rel\":\"self\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/0622f52f-0c95-41c1-ab5d-ee9bc335c839/name/cc994816-e03d-4674-a455-f1087b88e934\"},{\"rel\":\"full\",\"uri\":\"http://192.168.19.44/openmrs/ws/rest/v1/concept/0622f52f-0c95-41c1-ab5d-ee9bc335c839/name/cc994816-e03d-4674-a455-f1087b88e934?v=full\"}],\"resourceVersion\":\"1.9\"}],\"displayString\":\"\u09B8\u09AE\u09CD\u09AD\u09BE\u09AC\u09CD\u09AF \u09AF\u0995\u09CD\u09B7\u09CD\u09AE\u09BE\",\"resourceVersion\":\"2.0\",\"translationKey\":\"\u09B8\u09AE\u09CD\u09AD\u09BE\u09AC\u09CD\u09AF_\u09AF\u0995\u09CD\u09B7\u09CD\u09AE\u09BE_41\"},\"inactive\":false,\"groupMembers\":[]}],\"encounterTypeUuid\":\"81852aee-3f10-11e4-adec-0800271c1b75\"}\r\n");
+		HttpResponse op = HttpUtil.post(HttpUtil.removeEndingSlash(OPENMRS_BASE_URL) + "/" + BAHMNI_ENCOUNTER_URL, "",
 		    enc.toString(), OPENMRS_USER, OPENMRS_PWD);
 		return new JSONObject(op.body());
+	}
+
+	private JSONObject createEncounterJson(Event e) throws JSONException {
+		JSONObject pt = patientService.getPatientByIdentifier(e.getBaseEntityId());
+		Client client = clientService.getByBaseEntityId(e.getBaseEntityId());
+		JSONObject enc = new JSONObject();
+		JSONObject pr = userService.getPersonByUser(e.getProviderId());
+		
+		//Main JSON Structure
+		enc.put("encounterDatetime", OPENMRS_DATE.format(e.getEventDate().toDate()));
+		// patient must be existing in OpenMRS before it submits an encounter. if it doesnot it would throw NPE
+		enc.put("patientUuid", pt.getString("uuid"));
+		enc.put("locationUuid", e.getLocationId());
+		//enc.put("provider", pr.getString("uuid"));
+		//TODO enc.put("patientUuid", pt.getString("uuid"));
+		//enc.put("encounterType", e.getEventType());
+		//TODO enc.put("encounterTypeUuid", e.getEventType());
+		JSONObject dummbyObject = new JSONObject();
+		enc.put("context", dummbyObject);
+		enc.put("encounterUuid", JSONObject.NULL);
+		enc.put("visitUuid", JSONObject.NULL);
+		enc.put("disposition", JSONObject.NULL);
+		JSONArray dummnyArray = new JSONArray();
+		enc.put("orders", dummnyArray);
+		enc.put("drugOrders", dummnyArray);
+		enc.put("bahmniDiagnoses", dummnyArray);
+		JSONArray prov = new JSONArray();
+		JSONObject providerUUID = new JSONObject();
+		providerUUID.put("uuid", "313c8507-9821-40e4-8a70-71a5c7693d72");
+		prov.put(providerUUID);
+		enc.put("providers", prov);
+		enc.put("visitType", "OPD");
+		enc.put("encounterTypeUuid", "");
+		dummbyObject.put("mdrtbSpecimen", dummnyArray);
+		enc.put("extensions", dummbyObject);
+
+		//observations for Followup Disease Female and Male
+		JSONArray obar = null;
+		if (e.getEventType().equalsIgnoreCase("Followup Disease Female")) {
+			obar = createDiseaseJSON(dummnyArray);
+		}
+		enc.put("observations", obar);
+		return enc;
+	}
+
+	private JSONArray createDiseaseJSON(JSONArray dummnyArray)
+			throws JSONException {
+		JSONArray obar;
+		obar = new JSONArray();
+		JSONObject observationObject1 = new JSONObject();
+		JSONObject observationObject2 = new JSONObject();
+		JSONObject observationObject3 = new JSONObject();
+
+		JSONObject conceptObject = new JSONObject();
+		String uuid = "60155ecd-9084-410f-ab44-17b4d05f216d";
+		String name = "স্বাস্থ্য সেবা দেওয়া হয়েছে";
+		conceptObject.put("uuid", uuid);
+		conceptObject.put("name", name);
+		observationObject1.put("concept", conceptObject);
+
+		observationObject1.put("formNamespace", "Bahmni");
+		observationObject1.put("formFieldPath", "");
+		observationObject1.put("voided", false);
+		observationObject1.put("interpretation", JSONObject.NULL);
+		observationObject1.put("groupMembers", dummnyArray);
+		observationObject1.put("inactive", false);
+
+		JSONObject valueObject = new JSONObject();
+		JSONObject nameObject = new JSONObject();
+		nameObject.put("display", "");
+		nameObject.put("uuid", "");
+		nameObject.put("name", "");
+		nameObject.put("locale", "");
+		nameObject.put("localePreferred", "");
+		nameObject.put("conceptNameType", "");
+		nameObject.put("links", "");
+		valueObject.put("name", nameObject);
+		valueObject.put("uuid", "");
+		observationObject1.put("value", valueObject);
+		obar.put(observationObject1);
+		obar.put(observationObject2);
+		obar.put(observationObject3);
+		//JSONObject testObs = new JSONObject("{\r\n      \"concept\": {\r\n        \"uuid\": \"f8ca5471-4e76-4737-8ea4-7555f6d5af0f\",\r\n        \"name\": \"Blood Group\"\r\n      },\r\n      \"formNamespace\": \"Bahmni\",\r\n      \"formFieldPath\": \"test2.1/1-0\",\r\n      \"voided\": false,\r\n      \"value\": \"A+\",\r\n      \"interpretation\": null,\r\n      \"inactive\": false,\r\n      \"groupMembers\": []\r\n    },"); 
+		//obar.put(testObs);
+		//List<Obs> ol = e.getObs();
+		//logger.info("observationssssssssssssssss: " + ol.size() + " object String: " + ol.get(0).toString());
+		//for (Obs obs : ol) {
+			//if (obs.getFieldType().equalsIgnoreCase("concept")) {
+				/*JSONObject conceptObject = new JSONObject();
+				conceptObject.put("uuid", "");
+				conceptObject.put("name", "");
+				observationObject.put("concept", conceptObject);*/
+
+				//observationObject.put("value", client.getAttribute(""));
+				/*observationObject.put("formNamespace", obs.getFieldType());
+				observationObject.put("formFieldPath", obs.getFieldCode());
+				observationObject.put("voided", obs.getFieldDataType());
+				observationObject.put("inactive", obs.getParentCode());
+				observationObject.put("groupMembers", obs.getValues());
+				observationObject.put("interpretation", obs.getHumanReadableValues());
+
+				obar.put(observationObject);*/
+			//ss}
+		//}
+		return obar;
 	}
 	
 	public JSONObject buildUpdateEncounter(Event e) throws JSONException {
